cd2361ff12f0f5eddf9e289f31896fbfe56f11f6
Rob Levin <roblevintennis@gmail.com>
2020-08-24 20:27:46 -0700

    Fairly decent initial setup of base buttons

--- components/button/svelte/src/Button.svelte
+++ components/button/svelte/src/Button.svelte
@@ -1,7 +1,10 @@
 <style>
-.btn {
-	border: 0;
-	background-color: hotpink;
-}	
+  .btn {
+    border: 0;
+    display: inline-block;
+    border-radius: 3px;
+    background-color: var(--neutral-blue-grey-dark);
+  }
 </style>
-<button class="btn">Test Button</button>
+
+<button class="btn">Sign Into Your Account</button>
