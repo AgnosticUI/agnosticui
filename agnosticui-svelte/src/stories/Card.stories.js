79a8a9e2d2158ec42db623c6b9e4844ed60ad8ec
Rob Levin <roblevintennis@gmail.com>
2020-09-07 10:02:42 -0700

    Remove cruft and add comment

--- agnosticui-svelte/src/stories/Card.stories.js
+++ agnosticui-svelte/src/stories/Card.stories.js
@@ -7,24 +7,3 @@ export default {
 export const Card = () => ({
   Component: CardExample,
 });
-
-
-// const Template = ({ ...args }) => (
-//   <Card {...args}>
-//     <p>Some test content for our &ldquo;slot&rdquo;</p>
-//   </Card>
-// );
-
-// export const CardTest = Template.bind({});
-// CardTest.args = {
-// };
-
-// export const CardStacked = Template.bind({});
-// CardStacked.args = {
-//   isStacked: true,
-// };
-
-// export const CardNoSkin = Template.bind({});
-// CardNoSkin.args = {
-//   isSkinned: false,
-// };
