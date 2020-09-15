f3a8c9dab97b5c84c47b8e109d8538b3751e86df
Rob Levin <roblevintennis@gmail.com>
2020-09-15 07:19:19 -0700

    Adds Svelte Header component

--- agnosticui-svelte/copystyles.js
+++ agnosticui-svelte/copystyles.js
@@ -54,3 +54,27 @@ css = fs.readFileSync('../agnosticui-css/card.css', 'utf8');
 const cardSvelte = fs.readFileSync('./src/stories/Card.svelte', 'utf8');
 const cardSvelteSynchronized = cardSvelte.replace(styleRegex, `<style>\n${css}\n</style>`);
 fs.writeFileSync('./src/stories/Card.svelte', cardSvelteSynchronized, 'utf8');
+
+
+/**
+ * Header 
+ */
+css = fs.readFileSync('../agnosticui-css/header.css', 'utf8');
+css = css.replace(/(.*header )(img)/, '$1:global($2)')
+css = css.replace(/(.*header-base )(img)/, '$1:global($2)')
+const headerSvelte = fs.readFileSync('./src/stories/Header.svelte', 'utf8');
+const headerSynchronized = headerSvelte.replace(styleRegex, `<style>\n${css}\n</style>`);
+fs.writeFileSync('./src/stories/Header.svelte', headerSynchronized, 'utf8');
+
+
+/**
+ * Header Navigation
+ */
+css = fs.readFileSync('../agnosticui-css/headernav.css', 'utf8');
+css = css.replace(/(.*header-nav )(a)/, '$1:global($2)')
+css = css.replace(/(.*header-nav )(ul)/, '$1:global($2)')
+css = css.replace(/(.*header-nav )(li)/, '$1:global($2)')
+css = css.replace(/(.*header-nav )(li:not\(:last-child\))/, '$1:global($2)')
+const headerNavSvelte = fs.readFileSync('./src/stories/HeaderNav.svelte', 'utf8');
+const headerNavSynchronized = headerNavSvelte.replace(styleRegex, `<style>\n${css}\n</style>`);
+fs.writeFileSync('./src/stories/HeaderNav.svelte', headerNavSynchronized, 'utf8');
---
+++ agnosticui-svelte/src/stories/HeaderExample.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Header from "./Header.svelte";
+  import HeaderNav from "./HeaderNav.svelte";
+</script>
+
+<Header isSticky={true}>
+  <div slot="logoleft">logo left</div>
+  <HeaderNav>
+    <ul>
+      <li><a href="#home">Home</a></li>
+      <li><a href="#about">About</a></li>
+    </ul>
+  </HeaderNav>
+  <div slot="logoright">logo right</div>
+</Header>
