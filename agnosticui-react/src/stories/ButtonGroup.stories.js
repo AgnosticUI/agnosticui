3e3c74b04b66865be07122039a435134129d91cd
Rob Levin <roblevintennis@gmail.com>
2020-09-16 23:42:12 -0700

    Refactors button group storybook stories

--- agnosticui-react/src/stories/ButtonGroup.stories.js
+++ agnosticui-react/src/stories/ButtonGroup.stories.js
@@ -6,39 +6,67 @@ export default {
   component: ButtonGroup,
 };
 
-const Template = (args) => (
-  <ButtonGroup>
-    <Button label="One"  {...args} />
-    <Button label="Two" {...args} />
-    <Button label="Three" {...args} />
-  </ButtonGroup>
+export const All = () => (
+  <>
+    <ButtonGroup>
+      <Button label="One" ariaLabel="Button One" />
+      <Button label="Two" ariaLabel="Button Two" />
+      <Button label="Three" ariaLabel="Button Three" />
+    </ButtonGroup>
+    <ButtonGroup>
+      <Button isRaised label="One" ariaLabel="Button One" />
+      <Button isRaised label="Two" ariaLabel="Button Two" />
+      <Button isRaised label="Three" ariaLabel="Button Three" />
+    </ButtonGroup>
+    <ButtonGroup>
+      <Button isBordered label="One" ariaLabel="Button One" />
+      <Button isBordered label="Two" ariaLabel="Button Two" />
+      <Button isBordered label="Three" ariaLabel="Button Three" />
+    </ButtonGroup>
+    <ButtonGroup>
+      <Button mode="secondary" label="One" ariaLabel="Button One" />
+      <Button mode="secondary" label="Two" ariaLabel="Button Two" />
+      <Button mode="secondary" label="Three" ariaLabel="Button Three" />
+    </ButtonGroup>
+    <ButtonGroup>
+      <Button mode="secondary" isRaised label="One" ariaLabel="Button One" />
+      <Button mode="secondary" isRaised label="Two" ariaLabel="Button Two" />
+      <Button mode="secondary" isRaised label="Three" ariaLabel="Button Three" />
+    </ButtonGroup>
+    <ButtonGroup>
+      <Button mode="secondary" isBordered label="One" ariaLabel="Button One" />
+      <Button mode="secondary" isBordered label="Two" ariaLabel="Button Two" />
+      <Button mode="secondary" isBordered label="Three" ariaLabel="Button Three" />
+    </ButtonGroup>
+    <ButtonGroup>
+      <Button mode="primary" label="One" ariaLabel="Button One" />
+      <Button mode="primary" label="Two" ariaLabel="Button Two" />
+      <Button mode="primary" label="Three" ariaLabel="Button Three" />
+    </ButtonGroup>
+    <ButtonGroup>
+      <Button mode="primary" isRaised label="One" ariaLabel="Button One" />
+      <Button mode="primary" isRaised label="Two" ariaLabel="Button Two" />
+      <Button mode="primary" isRaised label="Three" ariaLabel="Button Three" />
+    </ButtonGroup>
+    <ButtonGroup>
+      <Button mode="primary" isBordered label="One" ariaLabel="Button One" />
+      <Button mode="primary" isBordered label="Two" ariaLabel="Button Two" />
+      <Button mode="primary" isBordered label="Three" ariaLabel="Button Three" />
+    </ButtonGroup>
+    <ButtonGroup>
+      <Button mode="primary" isDisabled label="One" ariaLabel="Button One" />
+      <Button mode="primary" label="Two" ariaLabel="Button Two" />
+      <Button mode="primary" label="Three" ariaLabel="Button Three" />
+    </ButtonGroup>
+    <ButtonGroup>
+      <Button mode="primary" isDisabled label="One" ariaLabel="Button One" />
+      <Button mode="primary" label="Two" ariaLabel="Button Two" />
+      <Button mode="primary" isDisabled label="Three" ariaLabel="Button Three" />
+    </ButtonGroup>
+    <ButtonGroup>
+      <Button mode="primary" isDisabled label="One" ariaLabel="Button One" />
+      <Button mode="primary" isDisabled label="Two" ariaLabel="Button Two" />
+      <Button mode="primary" isDisabled label="Three" ariaLabel="Button Three" />
+    </ButtonGroup>
+  </>
 )
-
-export const ButtonGroupPrimary = Template.bind({});
-ButtonGroupPrimary.args = {
-  mode: "primary"
-};
-
-export const ButtonGroupPrimaryBordered = Template.bind({});
-ButtonGroupPrimaryBordered.args = {
-  isBordered: true,
-  mode: "primary"
-};
-
-export const ButtonGroupSecondary = Template.bind({});
-ButtonGroupSecondary.args = {
-  mode: "secondary"
-}
-
-export const ButtonGroupSecondaryBordered = Template.bind({});
-ButtonGroupSecondaryBordered.args = {
-  isBordered: true,
-  mode: "secondary"
-};
-
-export const ButtonGroupDefault = Template.bind({});
-
-export const ButtonGroupDefaultBordered = Template.bind({});
-ButtonGroupDefaultBordered.args = {
-  isBordered: true,
-};
