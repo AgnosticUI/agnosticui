b1d60018129cdc82d8aec97f0223a879a912d61b
Rob Levin <roblevintennis@gmail.com>
2020-09-16 18:06:13 -0700

    @storybook/addon-storysource added to svelte and vue. Some wip on vue ButtonDisabled trying to get storybook to do what I want it to. Refactors some of the react stories to be consolodated.

---
+++ agnosticui-vue/src/stories/ButtonsDisabled.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <AgnosticButton label="Default Disabled" isDisabled />
+    <AgnosticButton mode="primary" label="Primary Disabled" isDisabled />
+    <AgnosticButton mode="secondary" label="Secondary Disabled" isDisabled />
+    <AgnosticButton mode="secondary" label="Secondary Bordered Disabled" isBordered isDisabled />
+  </div>
+</template>
+
+<script>
+import AgnosticButton from "./Button.vue";
+export default {
+  name: "buttons-disabled",
+  components: { AgnosticButton },
+};
+</script>
