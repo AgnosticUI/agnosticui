3d11f543d34a7cb5c682794f1cff487c247b4238
Rob Levin <roblevintennis@gmail.com>
2020-10-03 18:24:56 -0700

    Removing a bunch of the -default agnosticui variables

--- showcase/src/partials/Colors.vue
+++ showcase/src/partials/Colors.vue
@@ -12,12 +12,14 @@
             The appropriate and
             <a
               href="https://medium.com/@raquel/inclusive-color-resources-for-your-a11y-color-needs-cf3448c63335"
-            >inclusive use of color</a>
+              >inclusive use of color</a
+            >
             can help to set the tone of your brand and encourage users to
             <a
               href="https://blog.adobe.com/en/publish/2017/03/29/the-impact-of-color-on-conversion-rates.html#gs.f5rtr2"
               class="href"
-            >take certain actions</a>.
+              >take certain actions</a
+            >.
           </h3>
         </FlexCol>
       </FlexRow>
@@ -25,18 +27,22 @@
         <FlexCol v-bind:xs="12">
           <div :class="$style.hotips">
             <p :class="$style.tip">
-              <span class="quoted">Colors express the main psychic functions of man.</span>
+              <span class="quoted"
+                >Colors express the main psychic functions of man.</span
+              >
               <a
                 href="https://www.facebook.com/cgjungny/posts/colors-express-the-main-psychic-functions-of-man-carl-gustav-jung/10155223465700093/"
                 class="href"
-              >Carl Gustav Jung</a>
+                >Carl Gustav Jung</a
+              >
             </p>
             <div :class="$style.tipcontent">
               <p>
-                AgnosticUI creates a minimal set of color tokens which you're encouraged to customize
-                and extend by simply redefining them in your top-level stylesheet. We use sensible
-                neutral gray color defaults, but intentionally use loud primary and secondary colors
-                in hopes that it will persuade you to define your own.
+                AgnosticUI creates a minimal set of color tokens which you're
+                encouraged to customize and extend by simply redefining them in
+                your top-level stylesheet. We use sensible neutral gray color
+                defaults, but intentionally use loud primary and secondary
+                colors in hopes that it will persuade you to define your own.
               </p>
 
               <p>Here's what the defaults look like if not redefined:</p>
@@ -48,7 +54,9 @@
                   </li>
                   <li :class="[$style.swatch, $style.secondary]">
                     <div :class="$style.body"></div>
-                    <footer :class="$style.footer">--agnosticui-secondary</footer>
+                    <footer :class="$style.footer">
+                      --agnosticui-secondary
+                    </footer>
                   </li>
                   <li :class="[$style.swatch, $style.light]">
                     <div :class="$style.body"></div>
@@ -60,15 +68,21 @@
                 <ul :class="$style.swatcheswrap">
                   <li :class="[$style.swatch, $style.grayextralight]">
                     <div :class="$style.body"></div>
-                    <footer :class="$style.footer">--agnosticui-gray-extra-light</footer>
+                    <footer :class="$style.footer">
+                      --agnosticui-gray-extra-light
+                    </footer>
                   </li>
                   <li :class="[$style.swatch, $style.graylight]">
                     <div :class="$style.body"></div>
-                    <footer :class="$style.footer">--agnosticui-gray-light</footer>
+                    <footer :class="$style.footer">
+                      --agnosticui-gray-light
+                    </footer>
                   </li>
                   <li :class="[$style.swatch, $style.graymid]">
                     <div :class="$style.body"></div>
-                    <footer :class="$style.footer">--agnosticui-gray-mid</footer>
+                    <footer :class="$style.footer">
+                      --agnosticui-gray-mid
+                    </footer>
                   </li>
                 </ul>
               </div>
@@ -76,11 +90,15 @@
                 <ul :class="$style.swatcheswrap">
                   <li :class="[$style.swatch, $style.graymiddark]">
                     <div :class="$style.body"></div>
-                    <footer :class="$style.footer">--agnosticui-gray-mid-dark</footer>
+                    <footer :class="$style.footer">
+                      --agnosticui-gray-mid-dark
+                    </footer>
                   </li>
                   <li :class="[$style.swatch, $style.graydark]">
                     <div :class="$style.body"></div>
-                    <footer :class="$style.footer">--agnosticui-gray-dark</footer>
+                    <footer :class="$style.footer">
+                      --agnosticui-gray-dark
+                    </footer>
                   </li>
                   <li :class="[$style.swatch, $style.dark]">
                     <div :class="$style.body"></div>
@@ -98,27 +116,27 @@
 /**
 * You should override these by simply defining them less the
 * `-default` in your top-level stylesheet. For example, to
-* override `--agnosticui-default-primary` simply define
+* override `--agnosticui-primary` simply define
 * `--agnosticui-primary` and that will take precedence.
 *
 * Under the hood, when AgnosticUI applies one of these it
 * does something like:
 *   margin-left: var(--agnosticui-primary,
-*                    var(--agnosticui-default-primary));
-* where the --agnosticui-default-primary is the fallback
+*                    var(--agnosticui-primary));
+* where the --agnosticui-primary is the fallback
 * if you haven't supplied --agnosticui-primary.
 */
 :root {
   /* Please, at mimimum redefine primary and secondary ;) */
-  --agnosticui-default-primary: Fuchsia;
-  --agnosticui-default-secondary: Tomato;
-  --agnosticui-default-gray-extra-light: #f8f8f8;
-  --agnosticui-default-gray-light: #e9e9e9;
-  --agnosticui-default-gray-mid: #d8d8d8;
-  --agnosticui-default-gray-mid-dark: #cccccc;
-  --agnosticui-default-gray-dark: #aaaaaa;
-  --agnosticui-default-dark: #333333;
-  --agnosticui-default-light: #ffffff;
+  --agnosticui-primary: Fuchsia;
+  --agnosticui-secondary: Tomato;
+  --agnosticui-gray-extra-light: #f8f8f8;
+  --agnosticui-gray-light: #e9e9e9;
+  --agnosticui-gray-mid: #d8d8d8;
+  --agnosticui-gray-mid-dark: #cccccc;
+  --agnosticui-gray-dark: #aaaaaa;
+  --agnosticui-dark: #333333;
+  --agnosticui-light: #ffffff;
 }
 
 /* You will override these by defining them in your top-level
