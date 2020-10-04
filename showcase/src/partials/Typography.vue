d265324059b66bb96e4fbd0eb24b97e7b4dfaf67
Rob Levin <roblevintennis@gmail.com>
2020-10-03 18:24:56 -0700

    Removing a bunch of the -default agnosticui variables

--- showcase/src/partials/Typography.vue
+++ showcase/src/partials/Typography.vue
@@ -8,7 +8,9 @@
       </FlexRow>
       <FlexRow v-bind:gutter="16">
         <FlexCol v-bind:xs="12">
-          <h3>Typography and scale are the foundations your designs are built on.</h3>
+          <h3>
+            Typography and scale are the foundations your designs are built on.
+          </h3>
         </FlexCol>
       </FlexRow>
       <FlexRow v-bind:gutter="16">
@@ -16,7 +18,11 @@
           <div :class="$style.hotips">
             <p :class="$style.tip">
               <span class="quoted">Make it easy to read.</span>
-              <a href="https://www.fastcompany.com/32656/web-sites-work" class="href">Roger Black</a>
+              <a
+                href="https://www.fastcompany.com/32656/web-sites-work"
+                class="href"
+                >Roger Black</a
+              >
             </p>
             <div :class="$style.tipcontent">
               Please consider heeding Black's 6 rules for web design:
@@ -26,31 +32,37 @@
                 <li :class="$style.li">Faster beats fancier</li>
                 <li :class="$style.li">Content is king</li>
                 <li :class="$style.li">Small bytes go down easier</li>
-                <li :class="$style.li">If you don’t have something good to say, don’t say anything</li>
+                <li :class="$style.li">
+                  If you don’t have something good to say, don’t say anything
+                </li>
               </ul>
               <h5 :class="$style.customize">Using CSS Custom to Theme</h5>
               <p>
                 <a
                   href="https://developer.mozilla.org/en-US/docs/Web/CSS/Using_CSS_custom_properties"
-                >CSS custom properties</a>
-                are an underutilized tool for web designers and developers alike. They allow us to define and use
-                <span
-                  class="quoted"
-                >variables</span> that can be shared across our app. But, even more powerfully, they
-                provide a built-in fallback system which allows us to replace default values with our own.
-                This makes theming easy!
+                  >CSS custom properties</a
+                >
+                are an underutilized tool for web designers and developers
+                alike. They allow us to define and use
+                <span class="quoted">variables</span> that can be shared across
+                our app. But, even more powerfully, they provide a built-in
+                fallback system which allows us to replace default values with
+                our own. This makes theming easy!
               </p>
               <p>
-                For example, AgnosticUI uses a system font stack by default, but you
-                may want to instead use your own. This can be achieved by simply defining
-                the custom css property for font family. Under the hood, AgnosticUI will do:
+                For example, AgnosticUI uses a system font stack by default, but
+                you may want to instead use your own. This can be achieved by
+                simply defining the custom css property for font family. Under
+                the hood, AgnosticUI will do:
               </p>
               <pre class="inline-code">
 var(--agnosticui-font-family,
-var(--agnosticui-default-font-family));</pre>
+var(--agnosticui-font-family));</pre
+              >
               <p>
-                Essentially, the left variable will be used if defined, otherwise it will fallback to
-                default one. All override variables in AgnosticUI are defined using this same naming
+                Essentially, the left variable will be used if defined,
+                otherwise it will fallback to default one. All override
+                variables in AgnosticUI are defined using this same naming
                 convention where you simply remove the
                 <i>--default</i>.
               </p>
@@ -67,19 +79,19 @@ var(--agnosticui-default-font-family));</pre>
  * suits you. Note the newlines here are for readability only.
  */
 :root {
-  --agnosticui-default-font-family: -apple-system, BlinkMacSystemFont,
+  --agnosticui-font-family: -apple-system, BlinkMacSystemFont,
   "Segoe UI", "Open Sans", "Ubuntu", "Fira Sans", Helvetica,
   "Droid Sans", "Helvetica Neue", sans-serif, "Apple Color Emoji",
   "Segoe UI Emoji", "Segoe UI Symbol";
-  --agnosticui-default-h1: 64px;
-  --agnosticui-default-h2: 48px;
-  --agnosticui-default-h3: 36px;
-  --agnosticui-default-h4: 24px;
-  --agnosticui-default-h5: 18px;
-  --agnosticui-default-h6: 14px;
-  --agnosticui-default-body: 16px;
-  --agnosticui-default-font-weight-bold: 600;
-  --agnosticui-default-font-weight-light: 300;
+  --agnosticui-h1: 64px;
+  --agnosticui-h2: 48px;
+  --agnosticui-h3: 36px;
+  --agnosticui-h4: 24px;
+  --agnosticui-h5: 18px;
+  --agnosticui-h6: 14px;
+  --agnosticui-body: 16px;
+  --agnosticui-font-weight-bold: 600;
+  --agnosticui-font-weight-light: 300;
 }
     </code></pre>
         </FlexCol>
