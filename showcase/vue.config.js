fb33175997d547c7448e43af8f13eb1890897a8b
Rob Levin <roblevinillustration@gmail.com>
2020-10-25 14:58:12 -0700

    Add untested deploy script.

--- showcase/vue.config.js
+++ showcase/vue.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/agnosticui/'
+    : '/',
   css: {
     requireModuleExtension: true,
     loaderOptions: {
