167d261f1a7517bfd6529ef9a0c66e653b2d2124
Rob Levin <roblevintennis@gmail.com>
2021-01-02 22:03:10 -0600

    Adds prettier configuration for the react package.

--- agnosticui-react/.eslintrc.js
+++ agnosticui-react/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:prettier/recommended"],
   parserOptions: {
     ecmaFeatures: {
       "jsx": true
