d6fea10b215923635cefd17b68698a7692228057
Rob Levin <roblevintennis@gmail.com>
2021-01-16 21:13:36 -0600

    Switch -- first draft of CSS example.

---
+++ agnosticui-css/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+    jest: true
+  },
+  plugins: [],
+  overrides: [],
+  rules: {
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-debugger': 'error',
+    'no-eval': 'error'
+  },
+  settings: {
+    react: {
+      version: 'detect' // React version
+    }
+  }
+}
---
+++ agnosticui-css/switch.js
@@ -0,0 +1,24 @@
+// In React, Vue, or Svelte we can probably do without the data attribute and just
+// scope this to the component without having to query all in the DOM
+const switchesElements = document.querySelectorAll('[data-agnostic-switch]')
+const handleClickEvent = (evt) => {
+  const el = evt.target
+  if (el.getAttribute('aria-pressed') == 'true') {
+    el.setAttribute('aria-pressed', 'false')
+  } else {
+    el.setAttribute('aria-pressed', 'true')
+  }
+}
+for (let i = 0; i < switchesElements.length; i++) {
+  switchesElements[i].addEventListener('click', handleClickEvent)
+
+  switchesElements[i].addEventListener('keypress', function (ev) {
+    var keyCode = ev.keyCode || ev.which
+    switch (keyCode) {
+      case 13:
+        ev.preventDefault()
+        ev.target.click()
+        break
+    }
+  })
+}
