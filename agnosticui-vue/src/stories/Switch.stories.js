1b7e11ee11f97be693a163e0765a68bf5a29edb7
Rob Levin <roblevintennis@gmail.com>
2021-01-21 19:09:20 -0600

    Fix uneccessary overflow hidden on the switch container. Adds sizes to vue switch.

--- agnosticui-vue/src/stories/Switch.stories.js
+++ agnosticui-vue/src/stories/Switch.stories.js
@@ -12,7 +12,17 @@ export default {
   components: [AgSwitch],
 };
 
+export const SwitchSmall = () => ({
+  components: { AgSwitch },
+  template: `<AgSwitch id="1" label="Switch small label" size="small" />`,
+});
+
 export const SwitchDefault = () => ({
   components: { AgSwitch },
-  template: `<AgSwitch id="1" label="Switch default label" />`,
+  template: `<AgSwitch id="2" label="Switch default label" />`,
+});
+
+export const SwitchLarge = () => ({
+  components: { AgSwitch },
+  template: `<AgSwitch id="3" label="Switch large label" size="large" />`,
 });
