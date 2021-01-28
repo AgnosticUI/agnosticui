e061834ac23c624bd2f2a714c2b6fe4b39af7da2
Rob Levin <roblevintennis@gmail.com>
2021-01-28 08:36:20 -0600

    Updates Svelte ButtonGroup to use storybook actions to track event handling in specs.

--- agnosticui-svelte/src/stories/ButtonGroupTests.svelte
+++ agnosticui-svelte/src/stories/ButtonGroupTests.svelte
@@ -1,35 +1,48 @@
 <script>
   import ButtonGroup from "./ButtonGroup.svelte";
   import Button from "./Button.svelte";
+  import { action } from "@storybook/addon-actions";
+
+  const actionsData = {
+    click: action("click"),
+  };
 </script>
 
 <ButtonGroup ariaLabel="Appropriate label for your button group">
-  <Button>One</Button>
-  <Button>Two</Button>
-  <Button>Three</Button>
+  <Button on:click="{actionsData.click}">One</Button>
+  <Button on:click="{actionsData.click}">Two</Button>
+  <Button on:click="{actionsData.click}">Three</Button>
 </ButtonGroup>
 
 <ButtonGroup ariaLabel="Appropriate label for your button group">
-  <Button mode="primary">One</Button>
-  <Button mode="primary">Two</Button>
-  <Button mode="primary">Three</Button>
+  <Button mode="primary" on:click="{actionsData.click}">One</Button>
+  <Button mode="primary" on:click="{actionsData.click}">Two</Button>
+  <Button mode="primary" on:click="{actionsData.click}">Three</Button>
 </ButtonGroup>
 
 <ButtonGroup ariaLabel="Appropriate label for your button group">
-  <Button mode="secondary">One</Button>
-  <Button mode="secondary">Two</Button>
-  <Button mode="secondary">Three</Button>
+  <Button mode="secondary" on:click="{actionsData.click}">One</Button>
+  <Button mode="secondary" on:click="{actionsData.click}">Two</Button>
+  <Button mode="secondary" on:click="{actionsData.click}">Three</Button>
 </ButtonGroup>
 
 <div style="margin-bottom: 40px;"></div>
 <ButtonGroup ariaLabel="Appropriate label for your button group">
-  <Button mode="primary" isBordered>One</Button>
-  <Button mode="primary" isBordered>Two</Button>
-  <Button mode="primary" isBordered>Three</Button>
+  <Button mode="primary" isBordered on:click="{actionsData.click}">One</Button>
+  <Button mode="primary" isBordered on:click="{actionsData.click}">Two</Button>
+  <Button mode="primary" isBordered on:click="{actionsData.click}">
+    Three
+  </Button>
 </ButtonGroup>
 
 <ButtonGroup ariaLabel="Appropriate label for your button group">
-  <Button mode="secondary" isBordered>One</Button>
-  <Button mode="secondary" isBordered>Two</Button>
-  <Button mode="secondary" isBordered>Three</Button>
+  <Button mode="secondary" isBordered on:click="{actionsData.click}">
+    One
+  </Button>
+  <Button mode="secondary" isBordered on:click="{actionsData.click}">
+    Two
+  </Button>
+  <Button mode="secondary" isBordered on:click="{actionsData.click}">
+    Three
+  </Button>
 </ButtonGroup>
