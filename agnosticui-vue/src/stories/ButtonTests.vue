23b6bf5e5eb6279911e1b16bbc181cea2833bb66
Rob Levin <roblevintennis@gmail.com>
2021-01-28 07:47:26 -0600

    Adds action storybook for tracking events in storybook for vue buttons and button groups.

--- agnosticui-vue/src/stories/ButtonTests.vue
+++ agnosticui-vue/src/stories/ButtonTests.vue
@@ -1,44 +1,44 @@
 <template>
   <div>
     <div>
-      <Button>Go</Button>
-      <Button :isBordered="true">Go</Button>
-      <Button isBlank>Go</Button>
-      <Button isBlock>Go</Button>
-      <Button isRounded>Go</Button>
-      <Button isRaised isRounded>Go</Button>
-      <Button isBordered isRounded>Go</Button>
-      <Button isCircle isBordered>Go</Button>
+      <Button @click="clickHandler">Go</Button>
+      <Button :isBordered="true" @click="clickHandler">Go</Button>
+      <Button isBlank @click="clickHandler">Go</Button>
+      <Button isBlock @click="clickHandler">Go</Button>
+      <Button isRounded @click="clickHandler">Go</Button>
+      <Button isRaised isRounded @click="clickHandler">Go</Button>
+      <Button isBordered isRounded @click="clickHandler">Go</Button>
+      <Button isCircle isBordered @click="clickHandler">Go</Button>
     </div>
     <div>
-      <Button mode="primary">Go</Button>
-      <Button mode="primary" :isBordered="true">Go</Button>
-      <Button mode="primary" isBlank>Go</Button>
-      <Button mode="primary" isBlock>Go</Button>
-      <Button mode="primary" isRounded>Go</Button>
-      <Button mode="primary" isRaised isRounded>Go</Button>
-      <Button mode="primary" isBordered isRounded>Go</Button>
-      <Button mode="primary" isCircle isBordered>Go</Button>
+      <Button mode="primary" @click="clickHandler">Go</Button>
+      <Button mode="primary" :isBordered="true" @click="clickHandler">Go</Button>
+      <Button mode="primary" isBlank @click="clickHandler">Go</Button>
+      <Button mode="primary" isBlock @click="clickHandler">Go</Button>
+      <Button mode="primary" isRounded @click="clickHandler">Go</Button>
+      <Button mode="primary" isRaised isRounded @click="clickHandler">Go</Button>
+      <Button mode="primary" isBordered isRounded @click="clickHandler">Go</Button>
+      <Button mode="primary" isCircle isBordered @click="clickHandler">Go</Button>
     </div>
     <div>
-      <Button mode="secondary">Go</Button>
-      <Button mode="secondary" :isBordered="true">Go</Button>
-      <Button mode="secondary" isBlank>Go</Button>
-      <Button mode="secondary" isBlock>Go</Button>
-      <Button mode="secondary" isRounded>Go</Button>
-      <Button mode="secondary" isRaised isRounded>Go</Button>
-      <Button mode="secondary" isBordered isRounded>Go</Button>
-      <Button mode="secondary" isCircle isBordered>Go</Button>
+      <Button mode="secondary" @click="clickHandler">Go</Button>
+      <Button mode="secondary" :isBordered="true" @click="clickHandler">Go</Button>
+      <Button mode="secondary" isBlank @click="clickHandler">Go</Button>
+      <Button mode="secondary" isBlock @click="clickHandler">Go</Button>
+      <Button mode="secondary" isRounded @click="clickHandler">Go</Button>
+      <Button mode="secondary" isRaised isRounded @click="clickHandler">Go</Button>
+      <Button mode="secondary" isBordered isRounded @click="clickHandler">Go</Button>
+      <Button mode="secondary" isCircle isBordered @click="clickHandler">Go</Button>
     </div>
 
     <div>
-      <Button :isDisabled="true">Go</Button>
+      <Button :isDisabled="true" @click="clickHandler">Go</Button>
       <Button mode="primary" isDisabled>Go</Button>
       <Button mode="secondary" isDisabled>Go</Button>
     </div>
     <div>
-      <Button size="small">Go</Button>
-      <Button size="large">Go</Button>
+      <Button size="small" @click="clickHandler">Go</Button>
+      <Button size="large" @click="clickHandler">Go</Button>
       <Button :isSkinned="false">No Skin</Button>
     </div>
   </div>
@@ -46,8 +46,15 @@
 
 <script>
 import Button from "./Button.vue";
+import { action } from '@storybook/addon-actions';
+
+const actionsData = {
+  clickHandler: action('click'),
+};
+
 export default {
-  name: "buttons-disabled",
+  name: "buttons-tests",
   components: { Button },
+  methods: actionsData
 };
 </script>
