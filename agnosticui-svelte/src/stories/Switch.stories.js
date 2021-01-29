3612dfb2bd7e4ac5857342783f832a34d0b03db3
Rob Levin <roblevintennis@gmail.com>
2021-01-29 13:50:30 -0600

    Adds other switches in svelte storybook.

--- agnosticui-svelte/src/stories/Switch.stories.js
+++ agnosticui-svelte/src/stories/Switch.stories.js
@@ -8,23 +8,110 @@ const actionsData = {
 export default {
   title: "Switch",
   component: Switch,
-  on: {
-    ...actionsData,
-  },
-  argTypes: {
-    id: { control: "string" },
-    label: { control: "string" },
-    size: { control: "string" },
-  },
 };
 
 const Template = ({ ...args }) => ({
   Component: Switch,
   props: args,
+  on: {
+    ...actionsData,
+  },
 });
+
+export const SwitchSmall = Template.bind({});
+SwitchSmall.args = {
+  id: "1",
+  label: "Switch small label",
+  size: "small",
+};
 
 export const SwitchDefault = Template.bind({});
 SwitchDefault.args = {
-  id: "1",
+  id: "2",
   label: "Switch default",
+};
+
+export const SwitchLarge = Template.bind({});
+SwitchLarge.args = {
+  id: "3",
+  label: "Switch large label",
+  size: "large",
+};
+
+export const SwitchPrechecked = Template.bind({});
+SwitchPrechecked.args = {
+  id: "4",
+  label: "Switch prechecked",
+  size: "large",
+  isChecked: true,
+};
+
+export const SwitchDisabled = Template.bind({});
+SwitchDisabled.args = {
+  id: "5",
+  label: "Switch disabled",
+  disabled: true,
+};
+
+export const SwitchAction = Template.bind({});
+SwitchAction.args = {
+  id: "99",
+  label: "Switch action",
+  isAction: true,
+};
+
+export const SwitchBorder = Template.bind({});
+SwitchBorder.args = {
+  id: "6",
+  label: "Switch border",
+  isBordered: true,
+};
+
+export const SwitchBorderAction = Template.bind({});
+SwitchBorderAction.args = {
+  id: "7",
+  label: "Switch border action",
+  isBordered: true,
+  isAction: true,
+};
+
+export const SwitchLabelRight = Template.bind({});
+SwitchLabelRight.args = {
+  id: "8",
+  label: "Switch with label on right",
+  labelPosition: "right",
+};
+
+export const SwitchLabelRightBorder = Template.bind({});
+SwitchLabelRightBorder.args = {
+  id: "9",
+  label: "Switch with label on right and border",
+  labelPosition: "right",
+  isBordered: true,
+};
+
+export const SwitchLabelRightAction = Template.bind({});
+SwitchLabelRightAction.args = {
+  id: "10",
+  label: "Switch with label on right action",
+  labelPosition: "right",
+  isAction: true,
+};
+export const SwitchLabelRightActionBorder = Template.bind({});
+SwitchLabelRightActionBorder.args = {
+  id: "11",
+  label: "Switch with label on right action",
+  labelPosition: "right",
+  isAction: true,
+  isBordered: true,
+};
+
+export const SwitchLabelRightActionBorderDisabled = Template.bind({});
+SwitchLabelRightActionBorderDisabled.args = {
+  id: "12",
+  label: "Switch with label on right action",
+  labelPosition: "right",
+  isAction: true,
+  isBordered: true,
+  disabled: true,
 };
