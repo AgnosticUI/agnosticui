0ec215cd3de754f0727f3b4d574924bfa8d58add
Rob Levin <roblevintennis@gmail.com>
2021-02-11 09:14:40 -0600

    Adds an a JavaScript example to the Progress component to show how to start and stop the value of said progress ui

--- showcase/src/partials/Progress.vue
+++ showcase/src/partials/Progress.vue
@@ -17,19 +17,42 @@
       </FlexRow>
       <FlexRow v-bind:gutter="12">
         <FlexCol v-bind:xs="12">
-          <h4>Progresss</h4>
+          <h4>Progress</h4>
         </FlexCol>
       </FlexRow>
       <FlexRow :customClasses="$style.rowProgresss" v-bind:gutter="12">
         <FlexCol v-bind:xs="12">
           <p class="flex">
-            <AgProgress value="35" max="100" />
+            <AgProgress :value="30" :max="100" />
           </p>
         </FlexCol>
       </FlexRow>
       <FlexRow v-bind:gutter="12">
         <FlexCol v-bind:xs="12">
           <pre v-highlightjs="regular"><code class="html"></code></pre>
+        </FlexCol>
+      </FlexRow>
+      <FlexRow v-bind:gutter="12">
+        <FlexCol v-bind:xs="12">
+          <h4>Progress with a bit of JavaScript</h4>
+          <p>The following example snippet uses Vue, but it is easily translated to
+            vanilla JavaScript or another framework.</p>
+        </FlexCol>
+      </FlexRow>
+      <FlexRow :customClasses="$style.rowProgresss" v-bind:gutter="12">
+        <FlexCol v-bind:xs="12">
+          <p class="flex">
+            <AgProgress :value="this.count" :max="100" />
+          </p>
+          <p style="margin-top: 2em;">
+            <button @click.prevent="startProgress" :disabled="this.started">Start</button>
+            <button @click.prevent="stopProgress" :disabled="!this.started">Stop</button>
+          </p>
+        </FlexCol>
+      </FlexRow>
+      <FlexRow v-bind:gutter="12">
+        <FlexCol v-bind:xs="12">
+          <pre v-highlightjs="javascript"><code class="vue"></code></pre>
         </FlexCol>
       </FlexRow>
     </FlexGrid>
@@ -49,9 +72,71 @@ export default {
     FlexGrid,
     FlexRow,
   },
+  methods: {
+    startProgress: function() {
+      if (this.count >= 100) {
+        this.count = 0
+      }
+      this.requestAnimationID = requestAnimationFrame(this.step)
+    },
+    stopProgress: function() {
+      if (this.requestAnimationID) {
+        cancelAnimationFrame(this.requestAnimationID);
+        this.requestAnimationID = undefined;
+        this.started = false;
+      }
+    },
+    step: function() {
+      this.started = true;
+      this.count += 1
+      if (this.count === 100) {
+        this.started = false;
+      } else if (this.count < 100) {
+        this.requestAnimationID = requestAnimationFrame(this.step)
+      }
+    },
+  },
   data() {
     return {
+      count: 0,
+      requestAnimationID: undefined,
+      started: false,
       regular: `<progress max="100" class="progress" value="35"></progress>`,
+      javascript: `<div>
+  <p>
+    <AgProgress :value="this.count" :max="100" />
+  </p>
+  <p>
+    <button @click.prevent="startProgress" :disabled="this.started">Start</button>
+    <button @click.prevent="stopProgress" :disabled="!this.started">Stop</button>
+  </p>
+</div>
+...etc.
+/* Vue methods to back the start / stop functionality */
+methods: {
+  startProgress: function() {
+    if (this.count >= 100) {
+      this.count = 0
+    }
+    this.requestAnimationID = requestAnimationFrame(this.step)
+  },
+  stopProgress: function() {
+    if (this.requestAnimationID) {
+      cancelAnimationFrame(this.requestAnimationID);
+      this.requestAnimationID = undefined;
+      this.started = false;
+    }
+  },
+  step: function() {
+    this.started = true;
+    this.count += 1
+    if (this.count === 100) {
+      this.started = false;
+    } else if (this.count < 100) {
+      this.requestAnimationID = requestAnimationFrame(this.step)
+    }
+  },
+},`
     };
   },
 };
