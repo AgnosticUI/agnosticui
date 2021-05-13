c8253cc78c815b9bb1b1ec264946412537fc4b61
Rob Levin <roblevintennis@gmail.com>
2021-05-13 17:35:21 -0500

    Adds copyStyles stuff for angular and temporarily points the storybook to buttonstorybook.css (until we get the agnostic button working and can fully nuke the storybook examples.)

--- agnosticui-angular/src/stories/button.component.ts
+++ agnosticui-angular/src/stories/button.component.ts
@@ -11,7 +11,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   >
     {{ label }}
   </button>`,
-  styleUrls: ['./button.css'],
+  styleUrls: ['./buttonstorybook.css'],
 })
 export default class ButtonComponent {
   /**
