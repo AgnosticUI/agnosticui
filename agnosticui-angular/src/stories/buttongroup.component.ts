b166c66ceb1e8d5082deefcb23b6da78266cc364
Rob Levin <roblevintennis@gmail.com>
2021-05-14 19:59:33 -0500

    Button groups.

---
+++ agnosticui-angular/src/stories/buttongroup.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'ag-button-group',
+  template: `<div [attr.role]="group" [attr.aria-label]="getAriaLabel" [ngClass]="classes">
+    <ng-content></ng-content>
+  </div>`,
+  styleUrls: ['./button.css'],
+})
+
+export default class ButtonGroup {
+  @Input() css?: string;
+  @Input() ariaLabel: string | undefined;
+  public get getAriaLabel(): string {
+    const label = this.ariaLabel ? this.ariaLabel : 'button group element';
+    return label;
+  }
+
+  public get classes(): string {
+    const cssClass = this.css ? this.css : '';
+    return [
+      'btn-group',
+      cssClass, 
+    ].join(' ');
+  }
+}
