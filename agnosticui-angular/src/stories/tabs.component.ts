4ccd3ff3294372549572c8a0778d5d3641240337
Rob Levin <roblevintennis@gmail.com>
2021-05-15 13:32:40 -0500

    Just setting up a not yet fully working angular tab component.

---
+++ agnosticui-angular/src/stories/tabs.component.ts
@@ -0,0 +1,43 @@
+import { AfterContentInit, Component, ContentChildren, QueryList } from '@angular/core';
+import Tab from './tab.component';
+
+@Component({
+  selector: 'tabs',
+  // TODO -- consider refactoring this to use Buttons so we get free a11y
+  template: `
+    <ul class="nav nav-tabs">
+      <li *ngFor="let tab of tabs" (click)="selectTab(tab)" [class.active]="tab.active">
+        <button>{{tab.tabTitle}}</button>
+      </li>
+    </ul>
+    <ng-content></ng-content>
+  `,
+  // TODO make this use external CSS instead
+  styles: [
+    `
+    .tab-close {
+      color: gray;
+      text-align: right;
+      cursor: pointer;
+    }
+    `
+  ]
+})
+
+export default class Tabs implements AfterContentInit {
+  @ContentChildren(Tab) tabs!: QueryList<Tab>;
+
+  ngAfterContentInit() {
+    console.log('tabs: ', this.tabs)
+    let activeTabs = this.tabs.filter((tab) => tab.active);
+    if (activeTabs.length === 0) {
+      this.selectTab(this.tabs.first);
+    }
+  }
+
+  selectTab(tab: Tab) {
+    // First deactivate all tabs, then select passed in tab
+    this.tabs.toArray().forEach(tab => tab.active = false);
+    tab.active = true;
+  }
+}
