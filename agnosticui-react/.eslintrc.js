aff9eb8b75790ee56f02c8c04f697cdddaa59cf0
Rob Levin <roblevintennis@gmail.com>
2021-09-12 08:00:03 -0500

    Improved react eslint and prettier

--- agnosticui-react/.eslintrc.js
+++ agnosticui-react/.eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:prettier/recommended"],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaFeatures: {
-      "jsx": true
+      jsx: true,
     },
-    ecmaVersion: 2020,
-    sourceType: 'module'
+    ecmaVersion: 11,
+    sourceType: 'module',
   },
   env: {
     es6: true,
@@ -13,21 +13,19 @@ module.exports = {
     node: true,
     jest: true,
   },
-  plugins: [
-    "react", "import"
-  ],
+  plugins: ['react', 'import'],
   overrides: [],
   rules: {
-    "no-console": ["error", { allow: ["warn", "error"] }],
-    "no-debugger": "error",
-    "no-eval": "error",
-    "import/first": "error",
-    "react/jsx-uses-react": "error",
-    "react/jsx-uses-vars": "error"
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-debugger': 'error',
+    'no-eval': 'error',
+    'import/first': 'error',
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
   },
   settings: {
     react: {
-      "version": "detect", // React version
+      version: 'detect', // React version
     },
-  }
+  },
 };
