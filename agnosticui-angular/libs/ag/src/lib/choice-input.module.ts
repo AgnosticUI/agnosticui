b3b7fb5054f36572f2ba7c295927077a4133f0ba
Rob Levin <roblevintennis@gmail.com>
2021-10-08 06:47:46 -0500

    Remove extraneous module

--- agnosticui-angular/libs/ag/src/lib/choice-input.module.ts
+++
@@ -1,9 +0,0 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { ChoiceInputComponent } from './choice-input.component';
-@NgModule({
-  imports: [CommonModule],
-  declarations: [ChoiceInputComponent],
-  exports: [ChoiceInputComponent],
-})
-export class ChoiceInputModule {}
