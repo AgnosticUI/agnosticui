f69a21d2b9986b4a03e151d606b85762606f64b1
Rob Levin <roblevintennis@gmail.com>
2021-10-23 19:02:40 -0500

    Adds dialog

---
+++ agnostic-css/public/js/dialog.js
@@ -0,0 +1,18 @@
+// https://github.com/KittyGiraudel/a11y-dialog
+
+/**
+ * This all depends on the CDN loaded a11y-dialog -- see _preview.hbs
+ */
+const element = document.getElementById('my-dialog')
+const html = document.documentElement
+const dialog = new A11yDialog(element)
+
+// Simple scroll lock -- https://a11y-dialog.netlify.app/advanced/scroll-lock
+dialog
+  .on('show', () => (html.style.overflowY = 'hidden'))
+  .on('hide', () => (html.style.overflowY = ''))
+
+dialog.on('show', function (dialogEl, triggerEl) {
+  console.log(dialogEl)
+  console.log(triggerEl)
+})
