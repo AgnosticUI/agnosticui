1b0639c80a5f9b27378c2b86510a554cc5f647e3
Rob Levin <roblevintennis@gmail.com>
2021-11-02 15:13:03 -0500

    Updates the custom button examples in React Tabs. a11y navigation should be fully working now I think.

--- agnostic-react/src/stories/TabButtonCustomExample.tsx
+++ agnostic-react/src/stories/TabButtonCustomExample.tsx
@@ -1,7 +1,7 @@
-import { ReactNode, FC } from 'react';
-import { Button } from '../Button';
+import { ReactNode, ReactElement, ForwardedRef, KeyboardEvent, forwardRef } from 'react';
 // We can override some of the boilerplate button styles by passing the `css` prop.
 import styles from './TabButtonCustom.module.css';
+import buttonStyles from '../button.module.css';
 
 export interface TabButtonCustomProps {
   css?: string;
@@ -13,28 +13,35 @@ export interface TabButtonCustomProps {
   // what corresponding panel should be revealed.
   // eslint-disable-next-line no-unused-vars
   onClick?: (activeIndex: number) => void;
+  onKeyDown: (ev: KeyboardEvent<HTMLElement>) => void;
 }
-
-export const TabButtonCustom: FC<TabButtonCustomProps> = ({
-  css = '',
-  index = 0,
-  selectedTab = 0,
-  onClick,
-  controlsPanelId,
-  children,
-}) => (
-  <Button
-    isBordered
-    key={`${index}`}
-    onClick={() => onClick && onClick(index)}
-    role="tab"
-    aria-controls={controlsPanelId}
-    tabIndex={selectedTab === index ? 0 : -1}
-    aria-selected={selectedTab === index}
-    css={`
-      ${styles.customTabButton} ${styles.active} ${css || ''}
-    `}
-  >
-    {children}
-  </Button>
+export const TabButtonCustom = forwardRef<HTMLButtonElement, TabButtonCustomProps>(
+  (
+    {
+      css = '',
+      index = 0,
+      selectedTab = 0,
+      onClick,
+      onKeyDown,
+      controlsPanelId,
+      children,
+    }: TabButtonCustomProps,
+    tabRef: ForwardedRef<HTMLButtonElement>,
+  ): ReactElement => (
+    <button
+      key={`${index}`}
+      className={`${buttonStyles.btn} ${buttonStyles.bordered} ${styles.customTabButton} ${
+        styles.active
+      } ${css || ''}`}
+      onClick={() => onClick && onClick(index)}
+      onKeyDown={onKeyDown}
+      ref={tabRef}
+      role="tab"
+      aria-controls={controlsPanelId}
+      tabIndex={selectedTab === index ? 0 : -1}
+      aria-selected={selectedTab === index}
+    >
+      {children}
+    </button>
+  ),
 );
