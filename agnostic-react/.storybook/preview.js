e5e159b657a38f968b848eda628114ea1d021b6c
Rob Levin <roblevintennis@gmail.com>
2021-11-08 12:18:12 -0600

    Adds a decorator so the correct agnosticui font family is used in React Storybook

--- agnostic-react/.storybook/preview.js
+++ agnostic-react/.storybook/preview.js
@@ -1,5 +1,11 @@
+import React from 'react';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" }
 };
+
+export const decorators = [
+  (Story) => <div style={{fontFamily: 'var(--agnostic-font-family)'}}><Story /></div>
+];
 
 require("!style-loader!css-loader!../src/css/common.min.css");
