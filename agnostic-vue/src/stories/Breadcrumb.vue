5ab50ed5a75836c0d0283b8abe7d7db7af260951
Rob Levin <roblevintennis@gmail.com>
2021-11-09 18:49:24 -0600

    Vue Breadcrumb component

--- agnostic-vue/src/stories/Breadcrumb.vue
+++ agnostic-vue/src/stories/Breadcrumb.vue
@@ -1,4 +1,73 @@
-
+<template>
+  <nav aria-label="breadcrumbs">
+    <ol :class="breadcrumbClasses">
+      <li
+        v-for="(route, index) of this.routes"
+        :key="index"
+        :class="crumbClasses(index)"
+      >
+        <a
+          v-if="index !== routes.length - 1 && route.url"
+          href={route.url}
+        >{{route.label}}</a>
+        <span v-else>{{route.label}}</span>
+      </li>
+    </ol>
+  </nav>
+</template>
+<script>
+export default {
+  name: "ag-breadcrumb",
+  props: {
+    routes: {
+      type: Array,
+      required: true,
+      validator: (value) => {
+        let isValid = true;
+        value.forEach(
+          (obj) =>
+            (isValid = Object.keys(obj).every((key) =>
+              ["label", "url"].includes(key)
+            ))
+        );
+        if (!isValid) {
+          console.warn(
+            "routes argument must be an array of objects with shape: {label:..., url:...}"
+          );
+        }
+        return isValid;
+      },
+    },
+    type: {
+      type: String,
+      default: "",
+      required: false,
+      validator: (value) => ["arrow", "slash", "bullet", ""].includes(value),
+    },
+  },
+  methods: {
+    isLast(crumbRoutes, idx) {
+      return idx === crumbRoutes.length - 1;
+    },
+    crumbClasses(index) {
+      console.log("yo?");
+      const isLastCrumb = this.isLast(this.routes, index);
+      return {
+        [this.$style["breadcrumb-item"]]: true,
+        [this.$style.active]: isLastCrumb,
+      };
+    },
+  },
+  computed: {
+    breadcrumbClasses() {
+      return {
+        [this.$style.breadcrumb]: true,
+        [this.$style[`breadcrumb-${this.type}`]]: !!this.type,
+      };
+    },
+  },
+};
+</script>
 <style module>
 .breadcrumb {
   display: flex;
@@ -15,7 +84,7 @@
 .breadcrumb-item + .breadcrumb-item::before {
   padding-inline-end: var(--fluid-6);
   color: var(--agnostic-gray-mid-dark);
-  content: '\203A';
+  content: "\203A";
 }
 
 .breadcrumb-item.active {
@@ -23,15 +92,14 @@
 }
 
 .breadcrumb-slash .breadcrumb-item + .breadcrumb-item::before {
-  content: '\0002f';
+  content: "\0002f";
 }
 
 .breadcrumb-arrow .breadcrumb-item + .breadcrumb-item::before {
-  content: '\02192';
+  content: "\02192";
 }
 
 .breadcrumb-bullet .breadcrumb-item + .breadcrumb-item::before {
-  content: '\02022';
+  content: "\02022";
 }
-
 </style>
