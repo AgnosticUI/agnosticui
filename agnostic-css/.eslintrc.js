f340db16b70a25889b187d297f1f8e041a0d1ffe
Rob Levin <roblevintennis@gmail.com>
2021-11-11 06:55:04 -0600

    Removes prettier from the agnostic-css package -- (favoring stylelint and --fix which were competing with prettier)

--- agnostic-css/.eslintrc.js
+++ agnostic-css/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  extends: ['eslint:recommended'],
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module'
