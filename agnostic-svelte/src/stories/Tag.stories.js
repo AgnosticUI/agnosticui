fc7b4a1873a69c3e944ff56b357cf5a086593daa
Rob Levin <roblevintennis@gmail.com>
2021-11-11 17:34:05 -0600

    Svelte Tags

---
+++ agnostic-svelte/src/stories/Tag.stories.js
@@ -0,0 +1,14 @@
+import Tags from './TagSlots.svelte';
+
+export default {
+  title: 'AG—Svelte (Beta)/Tag',
+  components: [Tags],
+};
+
+const Template = ({ ...args }) => ({
+  Component: Tags,
+  props: args,
+});
+
+export const All = Template.bind({});
+All.args = {}
