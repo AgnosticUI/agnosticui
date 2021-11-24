b17189648e1fe68e46a0c211506f0877dfe9b70d
Rob Levin <roblevintennis@gmail.com>
2021-11-24 17:02:41 -0600

    Gets rid of the agnostic-vue-OLD temp backup

--- agnostic-vue-OLD/.storybook/preview.js
+++
@@ -1,8 +0,0 @@
-
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
-
-require('!style-loader!css-loader!../src/css/common.min.css')
-
-// import("../src/css/common.min.css");
