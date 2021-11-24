33bcaad5d9694621003678dc99c594315ebfd8e8
Rob Levin <roblevintennis@gmail.com>
2021-11-24 17:35:17 -0600

    FIx some deployment stuff.

--- site/deploy.sh
+++ site/deploy.sh
@@ -4,10 +4,11 @@
 set -e
 
 # build
-npm run build
+npm run docs:build
 
 # navigate into the build output directory
-cd dist
+cd docs/.vitepress/dist
+
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
