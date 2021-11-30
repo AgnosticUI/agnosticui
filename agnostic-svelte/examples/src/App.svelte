4a0de4be54ca799685436bde0bc6eacc53dd445a
Rob Levin <roblevintennis@gmail.com>
2021-11-30 11:29:27 -0600

    Adds React Breadcrumbs docs and updates the examples app with breadcrumbs too.

--- agnostic-svelte/examples/src/App.svelte
+++ agnostic-svelte/examples/src/App.svelte
@@ -4,6 +4,7 @@
     Alert,
     Avatar,
     AvatarGroup,
+    Breadcrumb,
 		Button,
 		Card,
 		ChoiceInput,
@@ -44,8 +45,24 @@
 		},
 	];
   let alertMessage = 'Alerts should be used for timely information.';
+
+  const trailOfTennisRoutes = [
+    {
+      label: "Tennis",
+      url: "#tennis",
+    },
+    {
+      label: "Superstars",
+      url: "#tennis-superstars",
+    },
+    {
+      label: "Serena Williams",
+      url: "#tennis-superstars-serena",
+    },
+  ];
 </script>
 <style>
+  
 	.container {
 		margin: 2rem 5rem;
 	}
@@ -122,6 +139,27 @@
     </div>
   </Header>
 </div>
+<div class="container">
+  <h2 class="mbs40 mbe24">Breadcrumbs</h2>
+  <div class="mbs24 mbe16">
+    <Breadcrumb routes={ trailOfTennisRoutes } />
+    <Breadcrumb
+      type="slash"
+      routes={ trailOfTennisRoutes }
+    />
+    <Breadcrumb
+      type="bullet"
+      routes={ trailOfTennisRoutes }
+    />
+    <Breadcrumb
+      type="arrow"
+      routes={ trailOfTennisRoutes }
+    />
+    <Breadcrumb routes={[{ label: 'A single route will look *linkless*' }]} />
+    <Breadcrumb routes={[{label: 'First', url: '#foo'}, { label: 'Second', url: '#bar' }]} />
+  </div>
+</div>
+
 <div class="container">
   <h3 class="mbs40 mbe24">Tabs</h3>
   <Tabs size="large" tabs={[
