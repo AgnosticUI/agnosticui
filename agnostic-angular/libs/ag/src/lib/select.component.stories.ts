c87db809274f50ada22808b8d8539acd4ed55427
Rob Levin <roblevintennis@gmail.com>
2021-12-01 18:29:10 -0600

    Angular Select storybook stories.

--- agnostic-angular/libs/ag/src/lib/select.component.stories.ts
+++ agnostic-angular/libs/ag/src/lib/select.component.stories.ts
@@ -48,10 +48,98 @@ export const FirstOptionLabel = () => ({
   },
   template: `<p class="mbe24">You can pass in <code>defaultOptionLabel</code> to specify the first option's text copy (e.g. as we've done here with: &ldquo;Select your favorite tennis player&rdquo;)</p>
   <ag-select
-    uniqueId="sel1"
-    name="select1"
+    uniqueId="sel2"
+    name="select2"
     (changed)="propInput.changed($event)"
     defaultOptionLabel="Select your favorite tennis player"
+    labelCopy="Select the best tennis player of all time"
+    [options]="[
+      { value: 'andre', label: 'Andre Agassi' },
+      { value: 'serena', label: 'Serena Williams' },
+      { value: 'mac', label: 'John McEnroe' },
+      { value: 'borg', label: 'Bjorn Borg' },
+      { value: 'althea', label: 'Althea Gibson' },
+      { value: 'roger', label: 'Roger Federer' },
+    ]"
+  ></ag-select>`,
+});
+
+export const Disabled = () => ({
+  props: {
+    propInput: {
+      changed: actionsData.changed,
+    },
+  },
+  template: `<p class="mbe24">You can pass in <code>defaultOptionLabel</code> to specify the first option's text copy (e.g. as we've done here with: &ldquo;Select your favorite tennis player&rdquo;)</p>
+  <ag-select
+    uniqueId="sel3"
+    name="select3"
+    isDisabled="true"
+    labelCopy="Select the best tennis player of all time"
+    [options]="[
+      { value: 'andre', label: 'Andre Agassi' },
+    ]"
+  ></ag-select>`,
+});
+
+export const Small = () => ({
+  props: {
+    propInput: {
+      changed: actionsData.changed,
+    },
+  },
+  template: `<ag-select
+    uniqueId="sel4"
+    name="select4"
+    size="small"
+    (changed)="propInput.changed($event)"
+    labelCopy="Select the best tennis player of all time"
+    [options]="[
+      { value: 'andre', label: 'Andre Agassi' },
+      { value: 'serena', label: 'Serena Williams' },
+      { value: 'mac', label: 'John McEnroe' },
+      { value: 'borg', label: 'Bjorn Borg' },
+      { value: 'althea', label: 'Althea Gibson' },
+      { value: 'roger', label: 'Roger Federer' },
+    ]"
+  ></ag-select>`,
+});
+
+export const Large = () => ({
+  props: {
+    propInput: {
+      changed: actionsData.changed,
+    },
+  },
+  template: `<ag-select
+    uniqueId="sel5"
+    name="select5"
+    size="large"
+    (changed)="propInput.changed($event)"
+    labelCopy="Select the best tennis player of all time"
+    [options]="[
+      { value: 'andre', label: 'Andre Agassi' },
+      { value: 'serena', label: 'Serena Williams' },
+      { value: 'mac', label: 'John McEnroe' },
+      { value: 'borg', label: 'Bjorn Borg' },
+      { value: 'althea', label: 'Althea Gibson' },
+      { value: 'roger', label: 'Roger Federer' },
+    ]"
+  ></ag-select>`,
+});
+
+export const Multi = () => ({
+  props: {
+    propInput: {
+      changed: actionsData.changed,
+    },
+  },
+  template: `<ag-select
+    uniqueId="sel6"
+    name="select6"
+    isMultiple="true"
+    multipleSize="4"
+    (changed)="propInput.changed($event)"
     labelCopy="Select the best tennis player of all time"
     [options]="[
       { value: 'andre', label: 'Andre Agassi' },
