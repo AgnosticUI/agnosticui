bb610b278e8725cec0b55eb2dc46c5afb588e1c7
Rob Levin <roblevintennis@gmail.com>
2021-12-05 06:34:18 -0600

    Fix Vue Select -- multiple-size prop wasn't being bound.

--- agnostic-vue/src/stories/Select.stories.js
+++ agnostic-vue/src/stories/Select.stories.js
@@ -65,7 +65,7 @@ export const Large = () => ({
 export const Multiple = () => ({
   components: { AgSelect },
   template: `<div class="mbe16">
-    <ag-select :is-multiple="true" multiple-size="4" :options="[{ value: 'andre', label: 'Andre Agassi' }, { value: 'serena', label: 'Serena Williams'} , { value: 'mac', label: 'John McEnroe'}, { value: 'borg', label: 'Bjorn Borg'}, { value: 'althea', label: 'Althea Gibson'}, { value: 'roger', label: 'Roger Federer'}]"
+    <ag-select :is-multiple="true" :multiple-size="4" :options="[{ value: 'andre', label: 'Andre Agassi' }, { value: 'serena', label: 'Serena Williams'} , { value: 'mac', label: 'John McEnroe'}, { value: 'borg', label: 'Bjorn Borg'}, { value: 'althea', label: 'Althea Gibson'}, { value: 'roger', label: 'Roger Federer'}]"
     unique-id="sel1"
     name="select1"
     label-copy="Select the best tennis player of all time"
