34e6f7230d6a132562feb9a735bf21226a395dae
Rob Levin <roblevintennis@gmail.com>
2021-12-11 11:05:44 -0600

    Updates Angular build of common.min.css to have individual custom properties, resets, and utilities in own min.css files. Also, some typos from earlier in CSS got ported into Angular.

--- agnostic-angular/copystyles.js
+++ agnostic-angular/copystyles.js
@@ -12,6 +12,25 @@ let css = fs.readFileSync(
 fs.writeFileSync('./libs/ag/styles/common.min.css', css, 'utf8');
 
 /**
+ * Common styles broken up into individual modules so they can be imported individually
+ */
+css = fs.readFileSync(
+  '../agnostic-css/public/css-dist/common.properties.min.css',
+  'utf8'
+);
+fs.writeFileSync('./libs/ag/styles/common.properties.min.css', css, 'utf8');
+css = fs.readFileSync(
+  '../agnostic-css/public/css-dist/common.resets.min.css',
+  'utf8'
+);
+fs.writeFileSync('./libs/ag/styles/common.resets.min.css', css, 'utf8');
+css = fs.readFileSync(
+  '../agnostic-css/public/css-dist/common.utilities.min.css',
+  'utf8'
+);
+fs.writeFileSync('./libs/ag/styles/common.utilities.min.css', css, 'utf8');
+
+/**
  * Alert
  */
 css = fs.readFileSync('../agnostic-css/src/components/alert/alert.css', 'utf8');
