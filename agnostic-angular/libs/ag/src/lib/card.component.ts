be2215e230fccee8587f4cd4354d0178286bf78c
Rob Levin <roblevintennis@gmail.com>
2021-12-12 17:05:33 -0600

    Angular Card refactored with new card approach.

--- agnostic-angular/libs/ag/src/lib/card.component.ts
+++ agnostic-angular/libs/ag/src/lib/card.component.ts
@@ -10,15 +10,34 @@ import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 })
 export class CardComponent {
   @Input() css?: string;
+  @Input() type?: "success" | "info" | "warning" | "error";
   @Input() isAnimated?: boolean;
   @Input() isSkinned?: boolean = true;
   @Input() isStacked?: boolean;
+  @Input() isShadow?: boolean;
+  @Input() isRounded?: boolean;
+  @Input() isBorder?: boolean;
+
   public get classes(): string {
     // classes() {
     const baseClass = this.isSkinned ? 'card' : 'card-base';
     const isAnimatedClass = this.isAnimated ? 'card-animated' : '';
     const isStackedClass = this.isStacked ? 'card-stacked' : '';
+    const isShadowClass = this.isShadow ? 'card-shadow' : '';
+    const isRoundedClass = this.isRounded ? 'card-rounded' : '';
+    const isBorderClass = this.isBorder ? 'card-border' : '';
+    const typeClass = this.type ? `card-${this.type}` : '';
+
     const overrides = this.css ? `${this.css}` : '';
-    return [baseClass, isAnimatedClass, isStackedClass, overrides].join(' ');
+    return [
+      baseClass,
+      typeClass,
+      isAnimatedClass,
+      isStackedClass,
+      isShadowClass,
+      isRoundedClass,
+      isBorderClass,
+      overrides,
+    ].join(' ');
   }
 }
