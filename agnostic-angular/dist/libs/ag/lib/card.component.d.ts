25537fef4c91360b4bd6ae8d4e203b4ce96acfb2
Rob Levin <roblevintennis@gmail.com>
2021-12-12 17:05:33 -0600

    Angular Card refactored with new card approach.

--- agnostic-angular/dist/libs/ag/lib/card.component.d.ts
+++ agnostic-angular/dist/libs/ag/lib/card.component.d.ts
@@ -1,10 +1,14 @@
 import * as i0 from "@angular/core";
 export declare class CardComponent {
     css?: string;
+    type?: "success" | "info" | "warning" | "error";
     isAnimated?: boolean;
     isSkinned?: boolean;
     isStacked?: boolean;
+    isShadow?: boolean;
+    isRounded?: boolean;
+    isBorder?: boolean;
     get classes(): string;
     static ɵfac: i0.ɵɵFactoryDeclaration<CardComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<CardComponent, "ag-card", never, { "css": "css"; "isAnimated": "isAnimated"; "isSkinned": "isSkinned"; "isStacked": "isStacked"; }, {}, never, ["*"]>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<CardComponent, "ag-card", never, { "css": "css"; "type": "type"; "isAnimated": "isAnimated"; "isSkinned": "isSkinned"; "isStacked": "isStacked"; "isShadow": "isShadow"; "isRounded": "isRounded"; "isBorder": "isBorder"; }, {}, never, ["*"]>;
 }
--- agnostic-angular/libs/ag/src/lib/card.component.stories.ts
+++ agnostic-angular/libs/ag/src/lib/card.component.stories.ts
@@ -25,18 +25,62 @@ const Template: Story<CardComponent> = (args: CardComponent) => ({
       <div style="padding: 24px;">Default</div>
       <div style="padding: 24px;">Card</div>
     </ag-card>
-    <ag-card isStacked="true">
-      <div style="padding: 24px;">Stacked</div>
+    <div class="mbe24"></div>
+    <ag-card [isBorder]="true">
+      <div style="padding: 24px;">Border</div>
       <div style="padding: 24px;">Card</div>
     </ag-card>
-    <ag-card isStacked="true" isAnimated="true">
-      <div style="padding: 24px;">Stacked & Animated</div>
+    <div class="mbe24"></div>
+    <ag-card [isBorder]="true" [isRounded]="true">
+      <div style="padding: 24px;">Border and rounded</div>
       <div style="padding: 24px;">Card</div>
     </ag-card>
-    <ag-card isSkinned="false">
+    <div class="mbe24"></div>
+    <ag-card [isStacked]="true" [isBorder]="true">
+      <div style="padding: 24px;">Stacked and border</div>
+      <div style="padding: 24px;">Card</div>
+    </ag-card>
+    <div class="mbe24"></div>
+    <ag-card [isStacked]="true" [isShadow]="true">
+      <div style="padding: 24px;">Stacked and shadow</div>
+      <div style="padding: 24px;">Card</div>
+    </ag-card>
+    <div class="mbe24"></div>
+    <ag-card [isStacked]="true" [isShadow]="true" [isAnimated]="true">
+      <div style="padding: 24px;">Stacked, shadown, and animated</div>
+      <div style="padding: 24px;">Card</div>
+    </ag-card>
+    <div class="mbe24"></div>
+    <ag-card type="success" [isStacked]="true">
+      <div style="padding: 24px;">Success stacked</div>
+      <div style="padding: 24px;">Card</div>
+    </ag-card>
+    <div class="mbe24"></div>
+    <ag-card type="success" [isStacked]="true" [isRounded]="true">
+      <div style="padding: 24px;">Success, stacked, and rounded</div>
+      <div style="padding: 24px;">Card</div>
+    </ag-card>
+    <div class="mbe24"></div>
+    <ag-card type="info" [isStacked]="true">
+      <div style="padding: 24px;">Info and stacked</div>
+      <div style="padding: 24px;">Card</div>
+    </ag-card>
+    <div class="mbe24"></div>
+    <ag-card type="warning" [isStacked]="true">
+      <div style="padding: 24px;">Warning and stacked</div>
+      <div style="padding: 24px;">Card</div>
+    </ag-card>
+    <div class="mbe24"></div>
+    <ag-card type="error" [isStacked]="true">
+      <div style="padding: 24px;">Error and stacked</div>
+      <div style="padding: 24px;">Card</div>
+    </ag-card>
+    <div class="mbe24"></div>
+    <ag-card [isSkinned]="false">
       <div style="padding: 24px;">Base Card</div>
       <div style="padding: 24px;">No Skin</div>
     </ag-card>
+    <div class="mbe24"></div>
     <ag-card css="addition-classes">
       <div style="padding: 24px;">Custom CSS Class Overrides</div>
       <div style="padding: 24px;">Inspect to see additional-classes</div>
