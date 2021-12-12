83d6fadcc207e31e93d067bd6515b857d3861615
Rob Levin <roblevintennis@gmail.com>
2021-12-12 08:19:33 -0600

    React Card refactored twith new card approach.

--- agnostic-react/src/Card.tsx
+++ agnostic-react/src/Card.tsx
@@ -6,23 +6,40 @@ export interface CardProps extends HTMLAttributes<HTMLDivElement> {
   isSkinned?: boolean;
   isAnimated?: boolean;
   isStacked?: boolean;
+  // new stuff
+  isShadow?: boolean;
+  isBorder?: boolean;
+  isRounded?: boolean;
+  type?: 'success' | 'info' | 'warning' | 'error';
   children?: ReactNode;
 }
 
 export const Card: FC<CardProps> = ({
   isAnimated = false,
   isStacked = false,
+  isShadow = false,
+  isBorder = false,
+  isRounded = false,
   isSkinned = true,
   css = '',
+  type,
   children,
 }) => {
-  const klasses = [
+  // Type might be empty string so we only capitalize if it's truthy
+  const typeCapitalized = type ? `${type.slice(0, 1).toUpperCase()}${type.slice(1)}` : '';
+
+  const classes = [
     isSkinned ? styles.card : styles.cardBase,
     isAnimated ? styles.cardAnimated : '',
     isStacked ? styles.cardStacked : '',
+    isShadow ? styles.cardShadow : '',
+    isRounded ? styles.cardRounded : '',
+    isBorder ? styles.cardBorder : '',
+    type ? styles[`card${typeCapitalized}`] : '',
     css ? `${css}` : '',
-  ];
-  const classes = klasses.filter((klass) => klass.length).join(' ');
+  ]
+    .filter((cls) => cls.length)
+    .join(' ');
 
   return <div className={classes}>{children}</div>;
 };
