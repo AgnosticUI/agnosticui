f903afd8a8c47749dcce43168f09ec4857f7f88e
Rob Levin <roblevintennis@gmail.com>
2021-12-17 09:52:26 -0600

    Provides example of using the custom sortFn in React Storybook stories.

--- agnostic-react/src/stories/Table.stories.tsx
+++ agnostic-react/src/stories/Table.stories.tsx
@@ -21,19 +21,41 @@ const headers: TableHeaders[] = [
     key: 'slams',
     sortable: true,
   },
+  {
+    label: 'Birthdate',
+    key: 'birthdate',
+    sortable: true,
+    sortFn: (a: string, b: string) => {
+      // Naive date comparisons; but we're controlling data so ;-)
+      const d1 = new Date(a).getTime() || -Infinity;
+      const d2 = new Date(b).getTime() || -Infinity;
+      if (d1 > d2) {
+        return 1;
+      }
+      if (d1 < d2) {
+        return -1;
+      }
+      return 0;
+    },
+  },
 ];
 
-const createRow = (name, weapon, slams) => ({ name, weapon, slams });
+const createRow = (name, weapon, slams, birthdate) => ({ name, weapon, slams, birthdate });
 const rows: TableRows[] = [
-  createRow('Serena Williams', "Biggest serve in women's tennis all-time", 23),
-  createRow('Roger Federer', 'Forehand and serve', 20),
-  createRow('Andre Agassi', 'Return of serve. Groundstrokes', 8),
-  createRow('Steffi Graf', 'Forehand', 22),
-  createRow('Martina Navratilova', 'Serve and volley', 18),
-  createRow('Rafael Nadal', 'Backhand and speed', 20),
-  createRow('Althea Gibson', 'Speed, strength, and fluidity ', 11),
-  createRow('Novak Djokovic', 'Backhand and speed', 20),
-  createRow('Arthur Ashe', 'Serve and volley', 3),
+  createRow(
+    'Serena Williams',
+    "Biggest serve in women's tennis all-time",
+    23,
+    'September 26, 1981',
+  ),
+  createRow('Roger Federer', 'Forehand and serve', 20, 'August 8, 1981'),
+  createRow('Andre Agassi', 'Return of serve. Groundstrokes', 8, 'April 29, 1970'),
+  createRow('Steffi Graf', 'Forehand', 22, 'June 14, 1969'),
+  createRow('Martina Navratilova', 'Serve and volley', 18, 'October 18, 1956'),
+  createRow('Rafael Nadal', 'Backhand and speed', 20, 'June 3, 1986'),
+  createRow('Althea Gibson', 'Speed, strength, and fluidity ', 11, 'August 25, 1927'),
+  createRow('Novak Djokovic', 'Backhand and speed', 20, 'May 22, 1987'),
+  createRow('Arthur Ashe', 'Serve and volley', 3, 'July 10, 1943'),
 ];
 export const Default = () => <Table headers={headers} rows={rows} caption="Tennis Superstars" />;
 export const Small = () => (
