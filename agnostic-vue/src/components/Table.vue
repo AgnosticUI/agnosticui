21125d9697286fcb533392a74846ff71052cc739
Rob Levin <roblevintennis@gmail.com>
2021-12-19 16:12:43 -0600

    Vue table rows incorporated and implementing sorting too

--- agnostic-vue/src/components/Table.vue
+++ agnostic-vue/src/components/Table.vue
@@ -34,16 +34,29 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>Tbody cell 1</td>
-          <td>Tbody cell 2</td>
+        <tr
+          v-for="(row, i) in sortableItems"
+          :key="i"
+        >
+          <td
+            v-for="(key, cIndex) in Object.keys(row)"
+            :key="cIndex"
+          >
+            <div
+              v-if="headers[cIndex].renderFn"
+              v-html="headers[cIndex].renderFn(key, row[key])"
+            />
+            <div v-else>
+              {{ row[key] }}
+            </div>
+          </td>
         </tr>
       </tbody>
     </table>
   </div>
 </template>
 <script>
-import { ref, useCssModule } from "vue";
+import { ref, useCssModule, watch } from "vue";
 export default {
   name: "AgTable",
   props: {
@@ -107,12 +120,100 @@ export default {
       default: false,
     },
   },
-  setup() {
+  setup(props) {
     // https://v3.vuejs.org/api/global-api.html#usecssmodule
     const styles = useCssModule();
     // Essentially equivalent to React's useState('')
     let direction = ref("none");
     let sortingKey = ref("");
+
+    // Cache the unsorted rows for aria-sort="none" case
+    const originalRows = [...props.rows];
+    let sortableItems = ref([...originalRows]);
+
+    /**
+     * Plucks the columns from rows by key of the current sortingKey; sortingKey
+     * reflects the currently being sorted column due to user interaction e.g. they
+     * have clicked on that columns table header cell.
+     *
+     * Since we want to sort rows but by column comparisons, we need to "pluck out"
+     * these columns from the two rows. If we cannot find the columns in rows by the
+     * `sortingKey`, then we set these to `-Infinity` which places them at the bottom.
+     */
+    const pluckColumnToSort = (rowLeft, rowRight) => {
+      const colLeft =
+        rowLeft[sortingKey.value] === null ||
+        rowLeft[sortingKey.value] === undefined
+          ? -Infinity
+          : rowLeft[sortingKey.value];
+      const colRight =
+        rowRight[sortingKey.value] === null ||
+        rowRight[sortingKey.value] === undefined
+          ? -Infinity
+          : rowRight[sortingKey.value];
+      return {
+        colLeft,
+        colRight,
+      };
+    };
+
+    /**
+     * This function first checks if there is a corresponding custom sort function
+     * that was supplied in a header cell with key" of `sortingKey` named `.sortFn`
+     * per the API. If it finds, it will delegate to that for actual sort comparison.
+     * Otherwise, the function supplies its own fallback default (naive) sorting logic.
+     */
+    const internalSort = (rowLeft, rowRight) => {
+      let { colLeft, colRight } = pluckColumnToSort(rowLeft, rowRight);
+      /**
+       * First check if the corresponding header cell has a custom sort
+       * method. If so, we use that, else we proceed with our default one.
+       */
+      const headerWithCustomSortFunction = props.headers.find(
+        (h) => h.key === sortingKey.value && !!h.sortFn
+      );
+      if (headerWithCustomSortFunction && headerWithCustomSortFunction.sortFn) {
+        return headerWithCustomSortFunction.sortFn(colLeft, colRight);
+      }
+      // No custom sort method for the header cell, so we continue with our own.
+      // Strings converted to lowercase; dollar currency etc. stripped (not yet i18n safe!)
+      colLeft =
+        typeof colLeft === "string"
+          ? colLeft.toLowerCase().replace(/(^\$|,)/g, "")
+          : colLeft;
+      colRight =
+        typeof colRight === "string"
+          ? colRight.toLowerCase().replace(/(^\$|,)/g, "")
+          : colRight;
+      // If raw value represents a number explicitly set to Number
+      colLeft = !Number.isNaN(Number(colLeft)) ? Number(colLeft) : colLeft;
+      colRight = !Number.isNaN(Number(colRight)) ? Number(colRight) : colRight;
+      if (colLeft > colRight) {
+        return 1;
+      }
+      if (colLeft < colRight) {
+        return -1;
+      }
+      return 0;
+    };
+    // Simply flips the sign of results of the ascending sort
+    const descendingSort = (row1, row2) => internalSort(row1, row2) * -1;
+    watch([direction, sortingKey], (currentValue, oldValue) => {
+      console.log("current value: ", currentValue);
+      console.log("old value: ", oldValue);
+      const newDirection = currentValue[0];
+
+      // sortableItems.value = [...props.rows];
+      let rows = props.rows;
+      if (newDirection === "ascending") {
+        rows.sort(internalSort);
+      } else if (newDirection === "descending") {
+        rows.sort(descendingSort);
+      } else {
+        rows = [...originalRows];
+      }
+      sortableItems.value = rows;
+    });
 
     const getSortDirectionFor = (headerKey) => {
       if (sortingKey.value !== headerKey) {
@@ -121,6 +222,7 @@ export default {
         return direction.value;
       }
     };
+
     const getSortingClassesFor = (headerKey) => {
       if (sortingKey.value === headerKey) {
         return {
@@ -141,7 +243,6 @@ export default {
         direction.value = "none";
         sortingKey.value = headerKey;
       }
-
       switch (direction.value) {
         case "ascending":
           direction.value = "descending";
@@ -158,13 +259,13 @@ export default {
           );
       }
     };
-
     return {
       direction,
       getSortDirectionFor,
       getSortingClassesFor,
       handleSortClicked,
       sortingKey,
+      sortableItems,
     };
   },
   computed: {
@@ -175,7 +276,6 @@ export default {
           this.captionPosition !== "hidden",
       };
     },
-
     tableResponsiveClasses() {
       return {
         [this.$style["table-responsive"]]: !this.responsiveSize,
@@ -183,7 +283,6 @@ export default {
           !!this.responsiveSize,
       };
     },
-
     tableClasses() {
       return {
         [this.$style["table"]]: true,
@@ -200,7 +299,7 @@ export default {
 };
 </script>
 
-<style module>
+  <style module>
 .table {
   --table-bg: transparent;
   --table-accent-bg: transparent;
@@ -210,7 +309,6 @@ export default {
   --table-active-bg: rgb(0 0 0 / 1.5%);
   --table-hoverable-color: var(--agnostic-dark);
   --table-hoverable-bg: var(--agnostic-table-hover-bg, #f1faff);
-
   width: 100%;
   margin-bottom: var(--fluid-16);
   color: var(--agnostic-dark);
@@ -222,7 +320,6 @@ export default {
   padding: var(--fluid-8) var(--fluid-8);
   background-color: var(--table-bg);
   border-bottom-width: 1px;
-
   /* 4th is spread --table-accent-bg will gets reset for active, hover, striped */
   box-shadow: inset 0 0 0 9999px var(--table-accent-bg);
 }
@@ -265,9 +362,8 @@ export default {
 .caption-top {
   padding-block-start: var(--fluid-12);
   padding-block-end: var(--fluid-12);
-
   /* Takes writing mode into account -- (mdn) same as left if direction is left-to-right
-  and right if direction is right-to-left */
+    and right if direction is right-to-left */
   text-align: start;
 }
 
@@ -305,19 +401,16 @@ export default {
 
 .table-striped > tbody > tr:nth-of-type(odd) > * {
   --table-accent-bg: var(--table-striped-bg);
-
   color: var(--table-striped-color);
 }
 
 .table-active {
   --table-accent-bg: var(--table-active-bg);
-
   color: var(--table-active-color);
 }
 
 .table-hoverable > tbody > tr:hover > * {
   --table-accent-bg: var(--table-hoverable-bg);
-
   color: var(--table-hoverable-color);
 }
 
@@ -451,7 +544,6 @@ export default {
 .table-sort:focus {
   box-shadow: 0 0 0 var(--agnostic-focus-ring-outline-width)
     var(--agnostic-focus-ring-color);
-
   /* Needed for High Contrast mode */
   outline: var(--agnostic-focus-ring-outline-width)
     var(--agnostic-focus-ring-outline-style)
--- agnostic-vue/src/stories/Table.stories.js
+++ agnostic-vue/src/stories/Table.stories.js
@@ -5,54 +5,56 @@ export default {
   component: Table
 };
 
-/*
+const createRow = (name, weapon, slams, birthdate) => ({ name, weapon, slams, birthdate });
 
-const headers: TableHeaderCell[] = [
-  {
-    label: 'Name',
-    key: 'name',
-    sortable: true,
-  },
-  {
-    label: 'Weapon',
-    key: 'weapon',
-  },
-  {
-    label: 'Grand Slams',
-    key: 'slams',
-    sortable: true,
-  },
-  {
-    label: 'Birthdate',
-    key: 'birthdate',
-    sortable: true,
-    sortFn: (a: string, b: string) => {
-      // Naive date comparisons; but we're controlling data so ;-)
-      const d1 = new Date(a).getTime() || -Infinity;
-      const d2 = new Date(b).getTime() || -Infinity;
-      if (d1 > d2) {
-        return 1;
-      }
-      if (d1 < d2) {
-        return -1;
-      }
-      return 0;
-    },
-  },
-]; */
 export const Default = () => ({
   components: { Table },
   data() {
     return {
-      headers: [{
-        label: 'Name',
-        key: 'name',
-        sortable: true,
-      }, {
-        label: 'Weapon',
-        key: 'weapon',
-      }]
+      rows: [
+        createRow('Roger Federer', 'Forehand and serve', 20, 'August 8, 1981'),
+        createRow('Andre Agassi', 'Return of serve. Groundstrokes', 8, 'April 29, 1970'),
+        createRow('Steffi Graf', 'Forehand', 22, 'June 14, 1969'),
+        createRow('Martina Navratilova', 'Serve and volley', 18, 'October 18, 1956'),
+        createRow('Rafael Nadal', 'Backhand and speed', 20, 'June 3, 1986'),
+        createRow('Althea Gibson', 'Speed, strength, and fluidity ', 11, 'August 25, 1927'),
+        createRow('Novak Djokovic', 'Backhand and speed', 20, 'May 22, 1987'),
+        createRow('Arthur Ashe', 'Serve and volley', 3, 'July 10, 1943'),
+      ],
+      headers: [
+        {
+          label: 'Name',
+          key: 'name',
+          sortable: true,
+        },
+        {
+          label: 'Weapon',
+          key: 'weapon',
+        },
+        {
+          label: 'Grand Slams',
+          key: 'slams',
+          sortable: true,
+        },
+        {
+          label: 'Birthdate',
+          key: 'birthdate',
+          sortable: true,
+          sortFn: (a, b) => {
+            // Naive date comparisons; but we're controlling data so ;-)
+            const d1 = new Date(a).getTime() || -Infinity;
+            const d2 = new Date(b).getTime() || -Infinity;
+            if (d1 > d2) {
+              return 1;
+            }
+            if (d1 < d2) {
+              return -1;
+            }
+            return 0;
+          },
+        },
+      ]
     };
   },
-  template: `<Table :headers="headers" caption="My caption test" captionPosition="top" />`
+  template: `<Table :rows="rows" :headers="headers" caption="My caption test" captionPosition="top" />`
 });
