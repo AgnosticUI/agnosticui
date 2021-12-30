c50f254d60988970d5c3348a4565fb295498e756
Rob Levin <roblevintennis@gmail.com>
2021-12-30 07:26:07 -0600

    Bumps svelte,regenerates build, and updates snapshots.

--- agnostic-svelte/src/stories/Breadcrumb.svelte
+++ agnostic-svelte/src/stories/Breadcrumb.svelte
@@ -19,40 +19,39 @@
 </script>
 
 <style>
-.breadcrumb {
-  display: flex;
-  flex-wrap: wrap;
-  padding: 0;
-  white-space: nowrap;
-  list-style: none;
-}
+  .breadcrumb {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0;
+    white-space: nowrap;
+    list-style: none;
+  }
 
-.breadcrumb-item + .breadcrumb-item {
-  padding-inline-start: var(--fluid-6);
-}
+  .breadcrumb-item + .breadcrumb-item {
+    padding-inline-start: var(--fluid-6);
+  }
 
-.breadcrumb-item + .breadcrumb-item::before {
-  padding-inline-end: var(--fluid-6);
-  color: var(--agnostic-gray-mid-dark);
-  content: "\203A";
-}
+  .breadcrumb-item + .breadcrumb-item::before {
+    padding-inline-end: var(--fluid-6);
+    color: var(--agnostic-gray-mid-dark);
+    content: "\203A";
+  }
 
-.breadcrumb-item.active {
-  color: var(--agnostic-dark);
-}
+  .breadcrumb-item.active {
+    color: var(--agnostic-dark);
+  }
 
-.breadcrumb-slash .breadcrumb-item + .breadcrumb-item::before {
-  content: "\0002f";
-}
+  .breadcrumb-slash .breadcrumb-item + .breadcrumb-item::before {
+    content: "\0002f";
+  }
 
-.breadcrumb-arrow .breadcrumb-item + .breadcrumb-item::before {
-  content: "\02192";
-}
+  .breadcrumb-arrow .breadcrumb-item + .breadcrumb-item::before {
+    content: "\02192";
+  }
 
-.breadcrumb-bullet .breadcrumb-item + .breadcrumb-item::before {
-  content: "\02022";
-}
-
+  .breadcrumb-bullet .breadcrumb-item + .breadcrumb-item::before {
+    content: "\02022";
+  }
 </style>
 
 <nav aria-label="breadcrumbs">
