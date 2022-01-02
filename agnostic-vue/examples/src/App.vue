209a8e6022c5c9599303580dc25d3b26b38d5fe0
Rob Levin <roblevintennis@gmail.com>
2022-01-02 16:07:42 -0600

    Vue divider

--- agnostic-vue/examples/src/App.vue
+++ agnostic-vue/examples/src/App.vue
@@ -19,6 +19,7 @@ import {
   ChoiceInput,
   Close,
   Disclose,
+  Divider,
   EmptyState,
   Header,
   HeaderNav,
@@ -56,6 +57,7 @@ export default {
     ChoiceInput,
     Close,
     Disclose,
+    Divider,
     EmptyState,
     Header,
     HeaderNav,
@@ -1280,6 +1282,77 @@ export default {
           </AgButton>
         </template>
       </EmptyState>
+    </section>
+    <section>
+      <h2 class="mbe24">
+        Dividers
+      </h2>
+      <Divider />
+      <Divider>
+        <template #dividerContent>
+          Boom!
+        </template>
+      </Divider>
+      <Divider justify="start">
+        <template #dividerContent>
+          Start
+        </template>
+      </Divider>
+      <Divider justify="end">
+        <template #dividerContent>
+          End
+        </template>
+      </Divider>
+      <Divider size="small">
+        <template #dividerContent>
+          Small
+        </template>
+      </Divider>
+      <Divider size="large">
+        <template #dividerContent>
+          Large
+        </template>
+      </Divider>
+      <Divider size="xlarge">
+        <template #dividerContent>
+          XLarge
+        </template>
+      </Divider>
+      <Divider type="success">
+        <template #dividerContent>
+          Success
+        </template>
+      </Divider>
+      <Divider type="info">
+        <template #dividerContent>
+          Info
+        </template>
+      </Divider>
+      <Divider type="warning">
+        <template #dividerContent>
+          Warning
+        </template>
+      </Divider>
+      <Divider type="error">
+        <template #dividerContent>
+          Error
+        </template>
+      </Divider>
+      <div class="flex mbs40 mbe48">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <Divider is-vertical="true">
+          <template #dividerContent>
+            yes
+          </template>
+        </Divider>
+        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+        <Divider is-vertical="true">
+          <template #dividerContent>
+            sir!
+          </template>
+        </Divider>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
     </section>
     <section>
       <Disclose
---
+++ agnostic-vue/src/components/Divider.vue
@@ -0,0 +1,177 @@
+<template>
+  <div
+    role="separator"
+    :class="dividerClasses"
+    aria-orientation="horizontal"
+  >
+    <div
+      v-if="hasDividerContentSlot"
+      :class="$style['divider-content']"
+    >
+      <slot name="dividerContent" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "AgDivider",
+  props: {
+    isVertical: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    justify: {
+      type: String,
+      required: false,
+      default: "",
+      validator: (value) => ["start", "center", "end", ""].includes(value),
+    },
+    size: {
+      type: String,
+      required: false,
+      default: "",
+      validator: (value) => ["small", "large", "xlarge", ""].includes(value),
+    },
+    type: {
+      type: String,
+      required: false,
+      default: "",
+      validator: (value) =>
+        ["info", "success", "warning", "error", ""].includes(value),
+    },
+  },
+  computed: {
+    dividerClasses() {
+      return {
+        [this.$style["divider"]]: true,
+        [this.$style["divider-vertical"]]: !!this.isVertical,
+        [this.$style[`divider-justify-${this.justify}`]]: !!this.justify,
+        [this.$style[`divider-${this.size}`]]: !!this.size,
+        [this.$style[`divider-${this.type}`]]: !!this.type,
+      };
+    },
+    hasDividerContentSlot() {
+      return !!this.$slots.dividerContent;
+    },
+  },
+};
+</script>
+
+<style module>
+.divider {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  width: 100%;
+}
+
+.divider::before,
+.divider::after {
+  content: "";
+  background-color: var(--agnostic-gray-mid);
+  height: var(--fluid-2);
+  flex-grow: 1;
+}
+
+.divider-small::before,
+.divider-small::after {
+  height: 1px;
+}
+
+.divider-large::before,
+.divider-large::after {
+  height: var(--fluid-4);
+}
+
+.divider-xlarge::before,
+.divider-xlarge::after {
+  height: var(--fluid-6);
+}
+
+.divider-justify-end::after,
+.divider-justify-start::before {
+  flex-grow: 0;
+  flex-basis: 3%;
+}
+
+.divider-content {
+  padding-inline-start: var(--fluid-16);
+  padding-inline-end: var(--fluid-16);
+}
+
+.divider-vertical {
+  height: auto;
+  margin: 0 var(--fluid-16);
+  width: var(--fluid-16);
+  flex-direction: column;
+  align-self: stretch;
+}
+
+.divider-vertical::before,
+.divider-vertical::after {
+  width: var(--fluid-2);
+}
+
+.divider-vertical.divider-small::before,
+.divider-vertical.divider-small::after {
+  width: 1px;
+}
+
+.divider-vertical.divider-large::before,
+.divider-vertical.divider-large::after {
+  width: var(--fluid-4);
+}
+
+.divider-vertical.divider-xlarge::before,
+.divider-vertical.divider-xlarge::after {
+  width: var(--fluid-6);
+}
+
+.divider-vertical .divider-content {
+  padding-inline-start: var(--fluid-24);
+  padding-inline-end: var(--fluid-24);
+
+  /* Since we're vertical we need space from line above/below */
+  padding-block-start: var(--fluid-6);
+  padding-block-end: var(--fluid-6);
+}
+
+.divider-warning::before,
+.divider-warning::after {
+  background-color: var(--agnostic-warning-border-accent);
+}
+
+.divider-warning .divider-content {
+  color: var(--agnostic-warning-border-accent);
+}
+
+.divider-error::before,
+.divider-error::after {
+  background-color: var(--agnostic-error);
+}
+
+.divider-error .divider-content {
+  color: var(--agnostic-error);
+}
+
+.divider-success::before,
+.divider-success::after {
+  background-color: var(--agnostic-action);
+}
+
+.divider-success .divider-content {
+  color: var(--agnostic-action);
+}
+
+.divider-info::before,
+.divider-info::after {
+  background-color: var(--agnostic-primary);
+}
+
+.divider-info .divider-content {
+  color: var(--agnostic-primary);
+}
+
+</style>
