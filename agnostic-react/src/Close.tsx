9ae0f733154c91ba26b6573ac2c0b48903ae0425
Rob Levin <roblevintennis@gmail.com>
2022-01-09 22:07:08 -0600

    Refactors Close component to expose the close svg icon, the CSS classes, etc. Adds React Dialog component using a11y-dialog under hood. Regens react dist.

--- agnostic-react/src/Close.tsx
+++ agnostic-react/src/Close.tsx
@@ -5,22 +5,27 @@ export interface CloseProps {
   size?: 'small' | 'large' | 'xlarge' | '';
 }
 
-export const Close: FC<CloseProps> = ({ size = '' }): ReactElement => {
+export const CloseButtonIcon = () => (
+  <svg className={styles.close} viewBox="0 0 24 24" aria-hidden="true">
+    <path
+      fill="currentColor"
+      d="M.439 21.44a1.5 1.5 0 0 0 2.122 2.121l9.262-9.261a.25.25 0 0 1 .354 0l9.262 9.263a1.5 1.5 0 1 0 2.122-2.121L14.3 12.177a.25.25 0 0 1 0-.354l9.263-9.262A1.5 1.5 0 0 0 21.439.44L12.177 9.7a.25.25 0 0 1-.354 0L2.561.44A1.5 1.5 0 0 0 .439 2.561L9.7 11.823a.25.25 0 0 1 0 .354Z"
+    />
+  </svg>
+);
+
+export const closeButtonContainerClasses = ({ size = '' }: CloseProps): string => {
   // Type might be empty string so we only capitalize if it's truthy
   const sizeCapitalized = size ? `${size.slice(0, 1).toUpperCase()}${size.slice(1)}` : '';
 
   const closeClasses = [styles.closeButton, size ? styles[`closeButton${sizeCapitalized}`] : '']
     .filter((cls) => cls)
     .join(' ');
-
-  return (
-    <button className={closeClasses} aria-label="Close">
-      <svg className={styles.close} viewBox="0 0 24 24" aria-hidden="true">
-        <path
-          fill="currentColor"
-          d="M.439 21.44a1.5 1.5 0 0 0 2.122 2.121l9.262-9.261a.25.25 0 0 1 .354 0l9.262 9.263a1.5 1.5 0 1 0 2.122-2.121L14.3 12.177a.25.25 0 0 1 0-.354l9.263-9.262A1.5 1.5 0 0 0 21.439.44L12.177 9.7a.25.25 0 0 1-.354 0L2.561.44A1.5 1.5 0 0 0 .439 2.561L9.7 11.823a.25.25 0 0 1 0 .354Z"
-        />
-      </svg>
-    </button>
-  );
+  return closeClasses;
 };
+
+export const Close: FC<CloseProps> = ({ size = '' }): ReactElement => (
+  <button className={closeButtonContainerClasses({ size })} aria-label="Close">
+    <CloseButtonIcon />
+  </button>
+);
