9c3c2e747c04691ab10b6077d699ca866e721605
Rob Levin <roblevintennis@gmail.com>
2022-01-19 22:00:57 -0600

    Upgrades angular to 13 using nx migrations and fixes Storybook global CSS w/Angular 13 issue while at it.

--- agnostic-angular/.storybook/preview.js
+++ agnostic-angular/.storybook/preview.js
@@ -1,1 +1,4 @@
-require('!style-loader!css-loader!../libs/ag/styles/common.min.css');
+// https://github.com/storybookjs/storybook/pull/16980/files
+// https://github.com/storybookjs/storybook/issues/16950#issuecomment-1017085928
+// This is no longer working and the common.min.css is pulled in from angular.json
+// require('!style-loader!css-loader!../libs/ag/styles/common.min.css');
