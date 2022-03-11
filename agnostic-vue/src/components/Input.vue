3e7738415e2da1b0a2bc9d76465931cb821f2b9f
Rob Levin <roblevintennis@gmail.com>
2022-03-11 09:22:54 -0600

    Syncs the latest CSS (input label font-size no longer smaller then body font by default) across react, vue, angular (svelte already synced)

--- agnostic-vue/src/components/Input.vue
+++ agnostic-vue/src/components/Input.vue
@@ -296,11 +296,9 @@ export default {
   vertical-align: initial;
 }
 
-/* Reset labels and field errors to be 2px less then input font size */
+/* Reset field errors and help text to be 2px less then input font size */
 .field-help,
-.field-error,
-.label,
-.label-skin {
+.field-error {
   font-size: calc(var(--agnostic-font-size, 1rem) - 2px);
 }
 
@@ -423,10 +421,13 @@ export default {
 }
 
 .field-help-large,
-.field-error-large,
-.label-large {
+.field-error-large {
   /* We initially remove -2px from font-size so setting to font-size essentially adds the 2px back */
   font-size: var(--agnostic-font-size, 1rem);
+}
+
+.label-large {
+  font-size: calc(var(--agnostic-font-size, 1rem) + 0.25rem);
 }
 
 .input-small {
