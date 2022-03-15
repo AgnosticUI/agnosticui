6cbaaf36931bbe28c0495b8792ad9855b26e0ce7
Rob Levin <roblevintennis@gmail.com>
2022-03-15 11:13:16 -0500

    Adds 'role' prop for the Vue Drawer component so we can pass in alertdialog which can make the Drawer "sticky"

--- agnostic-vue/src/components/Drawer.vue
+++ agnostic-vue/src/components/Drawer.vue
@@ -4,6 +4,7 @@
     :dialog-root="drawerRoot"
     :drawer-placement="placement"
     :title-id="`${title.replaceAll(' ', '-').toLowerCase()}-id`"
+    :role="role"
     @instance="assignDrawerRef"
     close-button-label="Close drawer"
     :is-animation-fade-in="isAnimationFadeIn"
@@ -37,6 +38,20 @@ defineProps({
   title: {
     type: String,
     default: "",
+  },
+  /**
+   * Passing in 'alertdialog' for this can be used to make a "sticky" drawer such that
+   * ESC or clicking the overlay does NOT close the drawer.
+   *
+   * Blocked by: https://github.com/morkro/vue-a11y-dialog/issues/38
+   */
+  role: {
+    type: String,
+    required: false,
+    default: "dialog",
+    validator(value) {
+      return ["dialog", "alertdialog"].includes(value);
+    },
   },
   isAnimationFadeIn: {
     type: Boolean,
