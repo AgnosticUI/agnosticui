72a9860938ecff33834dcf2b4f91002ec08b1522
Rob Levin <roblevintennis@gmail.com>
2022-03-15 10:36:44 -0500

    Pull in latest svelte and vue in site docs page and updates the ChoiceInput page to show how you can toggle button e.g. "I have read TOS" use case

--- site/docs/components/ChoiceInputExamples.vue
+++ site/docs/components/ChoiceInputExamples.vue
@@ -1,14 +1,13 @@
-<script>
+<script setup>
 // Import the required AgnosticUI global common and component CSS
 import "agnostic-vue/dist/common.min.css";
 import "agnostic-vue/dist/index.css";
-import { ChoiceInput } from "agnostic-vue";
+import { Button, ChoiceInput } from "agnostic-vue";
+import { ref } from "vue";
 
-export default {
-  name: "ChoiceInputExamples",
-  components: {
-    ChoiceInput,
-  },
+let agreedToTos = ref(false);
+const toggleTosAgreedTo = () => {
+  agreedToTos.value = !agreedToTos.value;
 };
 </script>
 <template>
@@ -39,5 +38,25 @@ export default {
       :disabledOptions="['stones', 'isleybros']"
       :options="[{ name: 'bands', value: 'bonjovi', label: 'Bon Jovi'}, { name: 'bands', value: 'stones', label: 'Rolling Stones' }, { name: 'bands', value: 'isleybros', label: 'Isley Brothers' }, { name: 'bands', value: 'parliament', label: 'Parliament-Funkadelic' }]"
     />
+    <div class="mbs24 mbe24">
+      <p class="mbe16">Pass <code>false</code> to <code>:is-fieldset</code> (Vue) or
+        <code>isFieldset</code> (React, Svelte) to hide the fieldset from sighted
+        users. Also, we can use a reactive boolean ref to toggle button's <code>isDisabled</code>:
+      </p>
+      <ChoiceInput
+        type="checkbox"
+        :options="[
+          { name: 'tos', value: 'agrees', label: 'I have read and agree to the Terms of Service'},
+        ]"
+        legend-label="Large"
+        size="large"
+        :is-fieldset="false"
+        @change="toggleTosAgreedTo"
+      />
+    </div>
+    <Button
+      mode="primary"
+      :is-disabled="!agreedToTos"
+    >Submit</Button>
   </section>
 </template>
