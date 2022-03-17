006542ab7039a6bbb518211ba0b950a6c01d9487
Rob Levin <roblevintennis@gmail.com>
2022-03-17 11:30:33 -0500

    Refactoring svelte dialog and drawer to "lock down" global styles

---
+++ agnostic-svelte/src/lib/components/Drawer/Drawer.svelte
@@ -0,0 +1,36 @@
+<script>
+  import Dialog from "../Dialog/Dialog.svelte";
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+
+  let drawerInstance;
+  const assignDrawerRef = (ev) => {
+    drawerInstance = ev.detail.instance;
+    dispatch("instance", {
+      instance: drawerInstance,
+    });
+  };
+
+  export let id;
+  export let drawerRoot;
+  // dialog | alertdialog
+  export let placement;
+  export let title = "";
+  export let role = "dialog";
+  export let isAnimationFadeIn = true;
+</script>
+<Dialog
+  id={id}
+  dialogRoot={drawerRoot}
+  drawerPlacement={placement}
+  titleId="{`${title.replaceAll(' ', '-').toLowerCase()}-id`}"
+  role={role}
+  on:instance={assignDrawerRef}
+  isAnimationFadeIn={isAnimationFadeIn}
+  closeButtonLabel="Close drawer"
+>
+  <svelte:fragment slot="title">
+    <slot name="title" />
+  </svelte:fragment>
+  <slot />
+</Dialog>
