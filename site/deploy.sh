bc2088bf0d753b89eceacf387eae0e535001c63d
Rob Levin <roblevintennis@gmail.com>
2022-03-29 22:10:57 -0500

    comments documenting how to build the site while awaiting vitepress fix

--- site/deploy.sh
+++ site/deploy.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
 
+# While awaiting Vitepress issue to get sorted, I can actually use following workaround (kludgy as it is):
+# 1. Remove the `type: module` from agnostic-vue/package.json
+# 2. Rename files like `copystyles.js` to `copystyles.mjs` if get errors
+# 3. Run `npm run build && npm pack`
+# 4. Install in `/site` e.g. `npm i /Users/roblevin/workspace/opensource/agnosticui/agnostic-vue/agnostic-vue-1.0.20.tgz` provided building packed npm per previous step
+# 5. `npm run docs:build` should now work as will `./deploy.sh`
+# 6. **Do not forget to go back and set `type: module`
+
 # abort on errors
 set -e
 
