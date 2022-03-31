7dde2024b458649a8f52c11cb71db3fec9382492
Rob Levin <roblevintennis@gmail.com>
2022-03-31 15:47:18 -0500

    Adds the various Inputs in CSS components section of docs

--- site/docs/.vitepress/config.js
+++ site/docs/.vitepress/config.js
@@ -106,7 +106,7 @@ function getDocsSidebar() {
         { text: 'EmptyState', link: '/docs/css/empty-state' },
         { text: 'Header', link: '/docs/css/header' },
         { text: 'Icons', link: '/docs/css/icons' },
-        // { text: 'Inputs', link: '/docs/css/inputs' },
+        { text: 'Inputs', link: '/docs/css/inputs' },
         // { text: 'Loader', link: '/docs/css/loader' },
         // { text: 'Menu', link: '/docs/css/menu' },
         // { text: 'Pagination', link: '/docs/css/pagination' },
