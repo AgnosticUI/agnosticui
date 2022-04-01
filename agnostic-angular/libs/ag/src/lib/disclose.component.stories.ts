8106630307eb0f264d065a9c6efc67ef744fbc39
Rob Levin <roblevintennis@gmail.com>
2022-04-01 10:34:50 -0500

    Refactors Angular disclose.component to use the new isBordered disclose-border css.

--- agnostic-angular/libs/ag/src/lib/disclose.component.stories.ts
+++ agnostic-angular/libs/ag/src/lib/disclose.component.stories.ts
@@ -55,7 +55,6 @@ export const All = () => ({
       four Australian Open singles titles in the Open Era (Wikipedia).
     </ag-disclose>
   </div>
-
   <div>
     <h3>Background</h3>
     <p class="mbs16 mbe24">The earlier example favors using a &ldquo;clean&rdquo; if minimalist style. However,
@@ -77,6 +76,29 @@ export const All = () => ({
       third-most of all-time behind Margaret Court and Serena Williams (Wikipedia).
     </ag-disclose>
     <ag-disclose isBackground="true"  title="Andre Agassi">
+      Andre Kirk Agassi is an American former world No. 1 tennis player. He is an eight-time major champion and a 1996
+      Olympic gold medalist, as well as a runner-up in seven other Grand Slam tournaments. Agassi was the first man to win
+      four Australian Open singles titles in the Open Era (Wikipedia).
+    </ag-disclose>
+  </div>
+  <div>
+    <h3>Bordered</h3>
+    <ag-disclose isBordered="true" title="Roger Federer">
+      Roger Federer is a Swiss professional tennis player. He is ranked No. 11 in the world by the Association of Tennis
+      Professionals. He has won 20 Grand Slam men's singles titles, an all-time record shared with Rafael Nadal and Novak
+      Djokovic (Wikipedia).
+    </ag-disclose>
+    <ag-disclose isBordered="true"  title="Serena Williams">
+      Serena Jameka Williams is an American professional tennis player. She has won 23 Grand Slam singles titles, the most
+      by any player in the Open Era, and the second-most of all time behind Margaret Court. The Women's Tennis Association
+      ranked her singles world No. 1 on eight separate occasions between 2002 and 2017.(Wikipedia).
+    </ag-disclose>
+    <ag-disclose isBordered="true"  title="Stefi Graf">
+      Stefanie Maria Graf is a German former professional tennis player. She was ranked world No. 1 for a record 377 weeks
+      and won 22 Grand Slam singles titles, which is the second-most since the introduction of the Open Era in 1968 and
+      third-most of all-time behind Margaret Court and Serena Williams (Wikipedia).
+    </ag-disclose>
+    <ag-disclose isBordered="true"  title="Andre Agassi">
       Andre Kirk Agassi is an American former world No. 1 tennis player. He is an eight-time major champion and a 1996
       Olympic gold medalist, as well as a runner-up in seven other Grand Slam tournaments. Agassi was the first man to win
       four Australian Open singles titles in the Open Era (Wikipedia).
