ce13810e62a4f198e6ba29a540c9771da2171313
Rob Levin <roblevintennis@gmail.com>
2022-04-01 09:37:03 -0500

    Adds a bordered version of the Disclose CSS component

--- site/docs/.vitepress/theme/index.js
+++ site/docs/.vitepress/theme/index.js
@@ -1,7 +1,7 @@
 import DefaultTheme from 'vitepress/theme'
 
 // Global AgnosticUI CSS custom properties
-import 'agnostic-vue/dist/common.min.css'
+import 'agnostic-css/public/css-dist/common.min.css'
 import './highlightjs.css'
 import './custom.css'
 
