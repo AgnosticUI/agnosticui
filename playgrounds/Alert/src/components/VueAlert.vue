5f6e2cd735250fdc7588ca3c3e2bba4d75affc9e
Rob Levin <roblevintennis@gmail.com>
2022-04-03 09:25:06 -0500

    Updates astro alert example to have responsive typography and layout.

--- playgrounds/Alert/src/components/VueAlert.vue
+++ playgrounds/Alert/src/components/VueAlert.vue
@@ -1,92 +1,94 @@
 <template>
-  <div class="h4 mbe32 flex items-center">
-    <img
-      width="24"
-      height="24"
-      src="/assets/Vue-icon.svg"
-      alt="Vue logo"
-      class="mie12"
-    />Vue 3 Alerts
-  </div>
-  <div style="color: var(--agnostic-dark)">
-    <div class="mbs12 mbe16">
-      <Alert>Default</Alert>
+  <section class="mbe40">
+    <div class="h4 mbe32 flex items-center">
+      <img
+        width="24"
+        height="24"
+        src="/assets/Vue-icon.svg"
+        alt="Vue logo"
+        class="mie12"
+      />Vue 3 Alerts
+    </div>
+    <div style="color: var(--agnostic-dark)">
+      <div class="mbs12 mbe16">
+        <Alert>Default</Alert>
+      </div>
+      <div class="mbs12 mbe16">
+        <Alert isRounded>Rounded</Alert>
+      </div>
+      <div class="mbs12 mbe16">
+        <Alert isBorderAll>Border all</Alert>
+      </div>
+      <div class="mbs12 mbe16">
+        <Alert isBorderLeft>Border left</Alert>
+      </div>
     </div>
     <div class="mbs12 mbe16">
-      <Alert isRounded>Rounded</Alert>
+      <Alert type="dark">
+        <AlertIconExample
+          icon-type="dark"
+          utility-classes="mie8"
+        >
+          <template #icon />
+        </AlertIconExample>
+        <p class="flex-fill">
+          Dark alert with projected icon
+        </p>
+      </Alert>
     </div>
     <div class="mbs12 mbe16">
-      <Alert isBorderAll>Border all</Alert>
+      <Alert type="success">
+        <AlertIconExample
+          icon-type="success"
+          utility-classes="mie8"
+        >
+          <template #icon />
+        </AlertIconExample>
+        <p class="flex-fill">
+          Success alert with projected icon
+        </p>
+      </Alert>
     </div>
     <div class="mbs12 mbe16">
-      <Alert isBorderLeft>Border left</Alert>
+      <Alert type="info">
+        <AlertIconExample
+          icon-type="info"
+          utility-classes="mie8"
+        >
+          <template #icon />
+        </AlertIconExample>
+        <p class="flex-fill">
+          Info alert with projected icon
+        </p>
+      </Alert>
     </div>
-  </div>
-  <div class="mbs12 mbe16">
-    <Alert type="dark">
-      <AlertIconExample
-        icon-type="dark"
-        utility-classes="mie8"
-      >
-        <template #icon />
-      </AlertIconExample>
-      <p class="flex-fill">
-        Dark alert with projected icon
-      </p>
-    </Alert>
-  </div>
-  <div class="mbs12 mbe16">
-    <Alert type="success">
-      <AlertIconExample
-        icon-type="success"
-        utility-classes="mie8"
-      >
-        <template #icon />
-      </AlertIconExample>
-      <p class="flex-fill">
-        Success alert with projected icon
-      </p>
-    </Alert>
-  </div>
-  <div class="mbs12 mbe16">
-    <Alert type="info">
-      <AlertIconExample
-        icon-type="info"
-        utility-classes="mie8"
-      >
-        <template #icon />
-      </AlertIconExample>
-      <p class="flex-fill">
-        Info alert with projected icon
-      </p>
-    </Alert>
-  </div>
-  <div class="mbs12 mbe16">
-    <Alert type="warning">
-      <AlertIconExample
-        icon-type="warning"
-        utility-classes="mie8"
-      >
-        <template #icon />
-      </AlertIconExample>
-      <p class="flex-fill">
-        Warning alert with projected icon
-      </p>
-    </Alert>
-  </div>
-  <div class="mbs12 mbe16">
-    <Alert type="error">
-      <AlertIconExample
-        icon-type="error"
-        utility-classes="mie8"
-      >
-        <template #icon />
-      </AlertIconExample>
-      <p class="flex-fill">
-        Error alert with projected icon
-      </p>
-    </Alert>
-  </div>
+    <div class="mbs12 mbe16">
+      <Alert type="warning">
+        <AlertIconExample
+          icon-type="warning"
+          utility-classes="mie8"
+        >
+          <template #icon />
+        </AlertIconExample>
+        <p class="flex-fill">
+          Warning alert with projected icon
+        </p>
+      </Alert>
+    </div>
+    <div class="mbs12 mbe16">
+      <Alert type="error">
+        <AlertIconExample
+          icon-type="error"
+          utility-classes="mie8"
+        >
+          <template #icon />
+        </AlertIconExample>
+        <p class="flex-fill">
+          Error alert with projected icon
+        </p>
+      </Alert>
+    </div>
+  </section>
 </template>
 
 <script setup>
