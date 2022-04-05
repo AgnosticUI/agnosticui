8fd9f040176bd1877ccd74635524cf1fec2d31ce
Rob Levin <roblevintennis@gmail.com>
2022-04-05 16:38:28 -0500

    Adds Drawer Playground.

---
+++ playgrounds/Drawer/src/components/React.jsx
@@ -0,0 +1,87 @@
+import { useRef } from 'react';
+import "agnostic-react/dist/esm/index.css";
+import { Button, Drawer } from "agnostic-react";
+
+const props = ['drawer1', 'drawer2', 'drawer3', 'drawer4'].map((item) => {
+  return {
+    id: `test-${item}`,
+    dialogRoot: "#portal-root",
+    title: 'Test Drawer',
+    classNames: {
+      // Note we don't have to pass in ALL classNames props and those
+      // not included will fallback to react-a11y-dialog's defaults
+      // See https://github.com/KittyGiraudel/react-a11y-dialog#api
+      title: 'h3 mbe18 h4 mbe18 flex justify-center',
+    }
+  }
+})
+
+const drawer1PropsDefault = props[0];
+const drawer2PropsDefault = props[1];
+const drawer3PropsDefault = props[2];
+const drawer4PropsDefault = props[3];
+
+export default function React() {
+  // Handles to low-level drawer instances. Allows calling `show` & `hide`
+  const drawer1Ref = useRef();
+  const drawer2Ref = useRef();
+  const drawer3Ref = useRef();
+  const drawer4Ref = useRef();
+
+  return (
+    <section className="mbe40">
+      <div className="h4 mbe32 flex items-center">
+        <img width="24" height="24" src="/assets/React-icon.svg" alt="React logo" className="mie12" />React Drawer
+      </div>
+      <Button onClick={() => drawer1Ref.current.show()} type="button" mode="primary" isBordered isRounded isBlock>Open Drawer Top</Button>
+      <Drawer
+        {...drawer1PropsDefault}
+        drawerRef={drawerInstance => (drawer1Ref.current = drawerInstance)}
+        placement="top"
+      >
+        <p className="mbs16 mbe16">
+          default slot
+        </p>
+      </Drawer>
+      <div className="mbs16 mbe24" />
+      <Button onClick={() => drawer2Ref.current.show()} type="button" mode="primary" isBordered isRounded isBlock>Open Drawer Bottom</Button>
+      <Drawer
+        {...drawer2PropsDefault}
+        drawerRef={drawerInstance => (drawer2Ref.current = drawerInstance)}
+        placement="bottom"
+      >
+          <div className="flex-fill">
+          <p>This is main drawer slot. To test positioning, update the placement property to one of: start | end | top | bottom.</p>
+          <button
+            style={{position: 'absolute', bottom: '1rem', left: '1rem', right: '1rem'}}
+            onClick={() => drawer2Ref.current.hide()}
+          >
+            Close from within slot using instance
+          </button>
+        </div>
+      </Drawer>
+      <div className="mbs16 mbe24" />
+      <Button onClick={() => drawer3Ref.current.show()} type="button" mode="primary" isBordered isRounded isBlock>Open Drawer Start</Button>
+      <Drawer
+        {...drawer3PropsDefault}
+        drawerRef={drawerInstance => (drawer3Ref.current = drawerInstance)}
+        placement="start"
+      >
+        <p className="mbs16 mbe16">
+          default slot
+        </p>
+      </Drawer>
+      <div className="mbs16 mbe24" />
+      <Button onClick={() => drawer4Ref.current.show()} type="button" mode="primary" isBordered isRounded isBlock>Open Drawer End</Button>
+      <Drawer
+        {...drawer4PropsDefault}
+        drawerRef={drawerInstance => (drawer4Ref.current = drawerInstance)}
+        placement="end"
+      >
+        <p className="mbs16 mbe16">
+          default slot
+        </p>
+      </Drawer>
+    </section>
+  );
+}
---
+++ playgrounds/Drawer/src/components/Svelte.svelte
@@ -0,0 +1,134 @@
+<script>
+  import { Button, Drawer } from 'agnostic-svelte';
+  let drawer = null;
+  const openDrawer = () => {
+    if (drawer) {
+      drawer.show();
+    }
+  };
+
+  const closeDrawer = () => {
+    if (drawer) {
+      drawer.hide();
+    }
+  };
+
+  const assignDrawerRef = (ev) => {
+    drawer = ev.detail.instance;
+  };
+const props = ['drawer1', 'drawer2', 'drawer3', 'drawer4'].map((item) => {
+  return {
+    id: `test-${item}`,
+    dialogRoot: "#portal-root",
+    title: 'Test Drawer',
+    classNames: {
+      // Note we don't have to pass in ALL classNames props and those
+      // not included will fallback to react-a11y-dialog's defaults
+      // See https://github.com/KittyGiraudel/react-a11y-dialog#api
+      title: 'h3 mbe18 h4 mbe18 flex justify-center',
+    }
+  }
+})
+</script>
+<section>
+  <div class="h4 mbe32 flex items-center">
+    <img width="24" height="24" src="/assets/Svelte-icon.svg" alt="Svelte logo" class="mie12" />Svelte Drawer
+  </div>
+  <Button
+    type="button"
+    data-a11y-dialog-show="drawer-top-sveltetest"
+    mode="primary"
+    isBordered
+    isBlock
+    isRounded
+  >
+    Open Drawer Top
+  </Button>
+  <Drawer
+    id="drawer-top-sveltetest"
+    drawerRoot="#portal-root"
+    placement="top"
+    title="My Drawer Title"
+  >
+    <div class="flex-fill">
+      <p>This is main drawer slot. To test positioning, update the placement property to one of: start | end | top | bottom.</p>
+    </div>
+  </Drawer>
+  <div class="mbs24 mbe16" />
+  <Button
+    type="button"
+    data-a11y-dialog-show="drawer-bottom-sveltetest"
+    mode="primary"
+    isBordered
+    isBlock
+    isRounded
+  >
+    Open Drawer Bottom
+  </Button>
+  <Drawer
+    id="drawer-bottom-sveltetest"
+    drawerRoot="#portal-root"
+    placement="bottom"
+    title="My Drawer Title"
+    on:instance={assignDrawerRef}
+  >
+    <div class="flex-fill">
+      <p>This is main drawer slot. To test positioning, update the placement property to one of: start | end | top | bottom.</p>
+      <button
+        class="custom-close-button"
+        on:click={closeDrawer}
+      >
+        Close from within slot using instance
+      </button>
+    </div>
+  </Drawer>
+  <div class="mbs24 mbe16" />
+  
+  <div class="mbs24 mbe16" />
+  <Button
+    type="button"
+    data-a11y-dialog-show="drawer-start-sveltetest"
+    mode="primary"
+    isBordered
+    isBlock
+    isRounded
+  >
+    Open Drawer Start
+  </Button>
+  <Drawer
+    id="drawer-start-sveltetest"
+    drawerRoot="#portal-root"
+    placement="start"
+    title="My Drawer Title"
+  >
+    <div class="flex-fill">
+      <p>This is main drawer slot. To test positioning, update the placement property to one of: start | end | top | bottom.</p>
+    </div>
+  </Drawer>
+  <div class="mbs24 mbe16" />
+  <Button
+    type="button"
+    data-a11y-dialog-show="drawer-end-sveltetest"
+    mode="primary"
+    isBordered
+    isBlock
+    isRounded
+  >
+    Open Drawer End
+  </Button>
+  <Drawer
+    id="drawer-end-sveltetest"
+    drawerRoot="#portal-root"
+    placement="end"
+    title="My Drawer Title"
+  >
+    <div class="flex-fill" style="display: grid; grid-template-columns: 1fr; grid-template-rows: 100px 1fr; height: 50vh;">
+      <div style="background-color: var(--agnostic-primary)"></div>
+      <div style="background-color: var(--agnostic-action)"></div>
+      <p class="mbs40">Just testing some random use of CSS grid inside the drawer. No biggie.</p>
+    </div>
+  </Drawer>
+</section>
+
+<style>
+</style>
