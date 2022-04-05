c4cbc20533063659f1f56529a30dcd44987372ea
Rob Levin <roblevintennis@gmail.com>
2022-04-05 17:07:18 -0500

    Adds Header Playground

---
+++ playgrounds/Header/src/components/Svelte.svelte
@@ -0,0 +1,22 @@
+<script>
+	import {
+		Header,
+		HeaderNav,
+		HeaderNavItem,
+	} from 'agnostic-svelte';
+</script>
+<section>
+  <div class="h4 mbe32 flex items-center">
+    <img width="24" height="24" src="/assets/Svelte-icon.svg" alt="Svelte logo" class="mie12" />Svelte Header
+  </div>
+  <Header>
+		<div slot="logoleft">logo left</div>
+		<HeaderNav css="nav-overrides">
+			<HeaderNavItem><a href="#home">Home</a></HeaderNavItem>
+			<HeaderNavItem><a href="#products">Products</a></HeaderNavItem>
+			<HeaderNavItem><a href="#services">Services</a></HeaderNavItem>
+			<HeaderNavItem><a href="#about">About</a></HeaderNavItem>
+		</HeaderNav>
+		<div slot="logoright">logo right</div>
+	</Header> 
+</section>
---
+++ playgrounds/Header/src/components/Vue.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="mbe40">
+    <div class="h4 mbe32 flex items-center">
+      <img
+        width="24"
+        height="24"
+        src="/assets/Vue-icon.svg"
+        alt="Vue logo"
+        class="mie12"
+      />Vue 3 Header
+    </div>
+    <Header>
+      <template v-slot:logoleft>
+        <a href="https://web.dev/">web.dev</a>
+      </template>
+      <template v-slot:headernav>
+        <HeaderNav>
+          <HeaderNavItem>
+            <a href="https://css-tricks.com/">CSS-Tricks</a>
+          </HeaderNavItem>
+          <HeaderNavItem>
+            <a href="https://developer.mozilla.org/en-US/">MDN</a>
+          </HeaderNavItem>
+        </HeaderNav>
+      </template>
+      <template v-slot:logoright>
+        <a href="https://www.freecodecamp.org/">freeCodeCamp</a>
+      </template>
+    </Header>
+  </section>
+</template>
+
+<script setup>
+// Components CSS
+import "agnostic-vue/dist/index.css";
+import { Header, HeaderNav, HeaderNavItem } from "agnostic-vue";
+</script>
