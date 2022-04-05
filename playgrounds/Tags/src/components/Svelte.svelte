7a76398dd7bbe54a2aba5cfbc4bae029a1094d29
Rob Levin <roblevintennis@gmail.com>
2022-04-05 18:46:43 -0500

    Adds Tags Playground

---
+++ playgrounds/Tags/src/components/Svelte.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { Tag } from 'agnostic-svelte';
+</script>
+<section>
+  <div class="h4 mbe32 flex items-center">
+    <img width="24" height="24" src="/assets/Svelte-icon.svg" alt="Svelte logo" class="mie12" />Svelte Close
+  </div>
+  <Tag>unknown</Tag>
+  <Tag shape="round">Rounded</Tag>
+  <Tag shape="pill">Badge</Tag>
+  <Tag type="success" isUppercase="true">success</Tag>
+  <Tag type="info" isUppercase="true">info</Tag>
+  <Tag type="warning" isUppercase="true">warning</Tag>
+  <Tag type="error" isUppercase="true">error</Tag>
+  <Tag type="error" shape="circle">2</Tag>
+</section>
---
+++ playgrounds/Tags/src/components/Vue.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="mbe40">
+    <div class="h4 mbe32 flex items-center">
+      <img
+        width="24"
+        height="24"
+        src="/assets/Vue-icon.svg"
+        alt="Vue logo"
+        class="mie12"
+      />Vue 3 Close
+    </div>
+    <Tag class="mie6">unknown</Tag>
+    <Tag is-uppercase>
+      UpperCasE
+    </Tag>
+    <Tag
+      class="mie6"
+      shape="round"
+      is-uppercase
+    >
+      round
+    </Tag>
+    <Tag
+      class="mie6"
+      shape="pill"
+    >
+      pill badge
+    </Tag>
+    <Tag
+      class="mie6"
+      shape="circle"
+      type="error"
+    >
+      2
+    </Tag>
+    <Tag
+      class="mie6"
+      type="success"
+    >
+      success
+    </Tag>
+    <Tag
+      class="mie6"
+      type="info"
+    >
+      info
+    </Tag>
+    <Tag
+      class="mie6"
+      type="warning"
+    >
+      warning
+    </Tag>
+  </section>
+</template>
+
+<script setup>
+// Components CSS
+import "agnostic-vue/dist/index.css";
+import { Tag } from "agnostic-vue";
+</script>
