de12342ef8967b8ee89150f64d5c101e4aedb761
Rob Levin <roblevintennis@gmail.com>
2022-04-05 18:06:24 -0500

    Adds Loader Playground

---
+++ playgrounds/Loaders/src/components/Svelte.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { Loader } from 'agnostic-svelte';
+</script>
+<section>
+  <div class="h4 mbe32 flex items-center">
+    <img width="24" height="24" src="/assets/Svelte-icon.svg" alt="Svelte logo" class="mie12" />Svelte Close
+  </div>
+  <Loader size="small" />
+  <div class="mbe32" />
+  <Loader />
+  <div class="mbe32" />
+  <Loader size="large" />
+</section>
