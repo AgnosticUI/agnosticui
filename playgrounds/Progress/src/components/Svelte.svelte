92c13fa0825ee40c581cfb13651c7dcdc076e5dd
Rob Levin <roblevintennis@gmail.com>
2022-04-05 23:01:33 -0500

    Adds Progress playground

---
+++ playgrounds/Progress/src/components/Svelte.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { Progress } from 'agnostic-svelte';
+</script>
+<section>
+  <div class="h4 mbe32 flex items-center">
+    <img width="24" height="24" src="/assets/Svelte-icon.svg" alt="Svelte logo" class="mie12" />Svelte Close
+  </div>
+  <Progress value="30" max="100" />
+</section>
---
+++ playgrounds/Progress/src/components/Vue.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="mbe40">
+    <div class="h4 mbe32 flex items-center">
+      <img
+        width="24"
+        height="24"
+        src="/assets/Vue-icon.svg"
+        alt="Vue logo"
+        class="mie12"
+      />Vue 3 Close
+    </div>
+    <Progress
+      :value="30"
+      :max="100"
+    />
+  </section>
+</template>
+
+<script setup>
+// Components CSS
+import "agnostic-vue/dist/index.css";
+import { Progress } from "agnostic-vue";
+</script>
