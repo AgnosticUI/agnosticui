b04e58609d592998f525a955c5319a12ca7ef640
Andrew Vitale <vitale232@gmail.com>
2022-04-10 16:02:32 -0400

    feat(angular): impl menu that allows devs to pass their own content

--- agnostic-angular/libs/ag/src/lib/menu.component.ts
+++ agnostic-angular/libs/ag/src/lib/menu.component.ts
@@ -1,30 +1,37 @@
 /* eslint-disable @angular-eslint/no-host-metadata-property */
 import { DOCUMENT } from '@angular/common';
 import {
+  AfterContentInit,
   ChangeDetectionStrategy,
-  ChangeDetectorRef,
   Component,
+  ContentChild,
+  ContentChildren,
   ElementRef,
   EventEmitter,
   Inject,
+  InjectionToken,
   Input,
+  NgZone,
   OnDestroy,
   OnInit,
   Optional,
   Output,
   QueryList,
   ViewChild,
-  ViewChildren,
   ViewEncapsulation,
 } from '@angular/core';
-import { BehaviorSubject, fromEvent, of, Subject } from 'rxjs';
-import { switchMap, takeUntil } from 'rxjs/operators';
+import { BehaviorSubject, fromEvent, Subject } from 'rxjs';
+import { take, takeUntil } from 'rxjs/operators';
 import { MenuItemDirective } from './menu-item.directive';
+import { MenuContentDirective } from './menu-content.directive';
 
 export interface MenuItem {
   label: string;
   isDisabled?: boolean;
 }
+
+/** An Injection Token that can be used to inject a reference to the `MenuComponent` from child elements */
+export const AG_MENU = new InjectionToken<MenuComponent>('AG_MENU');
 
 @Component({
   selector: 'ag-menu',
@@ -37,60 +44,40 @@ export interface MenuItem {
       [class.menu-trigger-rounded]="isRounded"
       [class.menu-trigger-large]="size === 'large'"
       [class.menu-trigger-small]="size === 'small'"
-      (click)="onTriggerClick($event, selectedIndex ?? 0)"
-      (keyup)="onKeyup($event, selectedIndex ?? 0)"
+      (click)="onTriggerClick($event)"
+      (keyup)="onTriggerKeyUp($event)"
     >
       {{ menuTitle }}
       <span class="menu-icon" aria-hidden="true"> {{ icon }} </span>
     </button>
-    <div class="menu-items" role="menu" *ngIf="isExpanded$ | async">
-      <button
-        ag-menu-item
-        *ngFor="let item of menuItems; let idx = index"
-        [disabled]="item.isDisabled ?? false"
-        [isLarge]="size === 'large'"
-        [isSmall]="size === 'small'"
-        [isSelected]="idx === selectedIndex"
-        [isRounded]="isRounded"
-        (click)="handleItemClick(item, idx)"
-        (keydown)="onMenuItemKeyDown($event, idx)"
-      >
-        {{ item.label }}
-      </button>
+    <div class="menu-items" role="menu" *ngIf="_isExpanded$ | async">
+      <!-- <ng-content></ng-content> -->
+      <ng-container
+        [ngTemplateOutlet]="content?.templateRef ?? null"
+      ></ng-container>
     </div>
   `,
   styleUrls: ['./menu.css', './menu-item.css'],
-  styles: [
-    `
-      :host {
-        display: block;
-      }
-    `,
-  ],
   encapsulation: ViewEncapsulation.None, // Without it, positioning of the menu items is off
   changeDetection: ChangeDetectionStrategy.OnPush,
+  // Allow children of this `MenuComponent` in the DOM to inject a reference to it (e.g. `MenuItemDirective` does this.)
+  providers: [{ provide: AG_MENU, useExisting: MenuComponent }],
   host: {
     class: 'menu',
   },
 })
-export class MenuComponent implements OnInit, OnDestroy {
+export class MenuComponent implements AfterContentInit, OnInit, OnDestroy {
   // Emits when `ngOnDestroy` is called. When used with `takeUntil` operator,
   // has the effect of cleaning up event listeners (https://rxjs.dev/api/operators/takeUntil).
   #destroy$ = new Subject<void>();
 
   @Input() closeOnSelect = true;
-  private _closeOnClickOutside$ = new BehaviorSubject(true);
-  @Input() set closeOnClickOutside(val: boolean) {
-    this._closeOnClickOutside$.next(val);
-  }
-  get closeOnClickOutside() {
-    return this._closeOnClickOutside$.getValue();
-  }
+  @Input() closeOnClickOutside = true;
+
   @Input() icon = '▾';
   @Input() isBordered = false;
   @Input() isRounded = false;
   @Input() id?: string;
-  @Input() menuItems: MenuItem[] = [];
   @Input() menuTitle = 'Menu Title';
   @Input() size: 'small' | 'large' | undefined | null | '' = null;
 
@@ -100,65 +87,93 @@ export class MenuComponent implements OnInit, OnDestroy {
   @ViewChild('menuTrigger', { static: true, read: ElementRef })
   private triggerEl!: ElementRef<HTMLButtonElement>;
 
-  @ViewChildren(MenuItemDirective)
+  @ContentChild(MenuContentDirective) content!: MenuContentDirective;
+  @ContentChildren(MenuItemDirective, { descendants: false })
   private menuItemEls!: QueryList<MenuItemDirective>;
 
+  _isExpanded$ = new BehaviorSubject(false);
   get isExpanded() {
     // Exposing the `BehaviorSubject` value as a class property for consumers
-    return this.isExpanded$.getValue();
+    return this._isExpanded$.getValue();
   }
 
-  isExpanded$ = new BehaviorSubject(false);
-  selectedIndex: number | null = null;
-
   constructor(
-    private cdr: ChangeDetectorRef,
+    private ngZone: NgZone,
     private hostEl: ElementRef,
     @Optional() @Inject(DOCUMENT) private _doc?: Document
   ) {}
 
   ngOnInit(): void {
-    this._closeOnClickOutside$
-      .pipe(
-        takeUntil(this.#destroy$),
-        switchMap((shouldClose) =>
-          this._doc && shouldClose === true
-            ? fromEvent(this._doc, 'click')
-            : of(null)
-        )
-      )
-      .subscribe({
-        next: (e) => this.handleOutsideClicks(e),
-        error: console.error,
-      });
+    if (this._doc) {
+      fromEvent(this._doc, 'click')
+        .pipe(takeUntil(this.#destroy$))
+        .subscribe({
+          next: (e) => this.handleOutsideClicks(e),
+          error: console.error,
+        });
+    }
   }
 
   ngOnDestroy(): void {
     this.#destroy$.next();
   }
 
-  onTriggerClick(event: MouseEvent, index: number) {
-    this.isExpanded$.next(!this.isExpanded$.getValue());
-    this.triggerClick.emit(event);
-    // To handle the case where _the menu was just expanded by toggling the behavior subject_,
-    // force Angular to synchronously run change detection. This will force the runtime
-    // to render the menu items, so we're sure the `ViewChildren` will exist.
-    // Note this is required since we're lying to the compiler when declaring `ViewChildren` with the `!` symbol.
-    // The compiler fib and CDR hack allows us to avoid the complexity of eventing render cycles
-    this.cdr.detectChanges();
-    this.focusMenuItem(index ?? 0);
-  }
-
-  handleItemClick(item: MenuItem, index: number) {
-    this.selectedIndex = index;
-    this.selectionChange.emit(item);
-    if (this.closeOnSelect) {
-      this.isExpanded$.next(false);
-      this.triggerEl.nativeElement.focus();
+  ngAfterContentInit(): void {
+    if (!this.content) {
+      console.warn(
+        'AgnosticUI Angular `MenuComponent` did not discover the `agMenuContent` attribute in your template. No template to render! ' +
+          'A simple MenuItems template may look something like the following: ```html\n<ng-template agMenuContent>\n  <button ag-menu-item>Neat!</button>\n</ng-template>\n```'
+      );
     }
   }
 
-  onKeyup(ev: KeyboardEvent, index: number): void {
+  onTriggerClick(event: MouseEvent) {
+    this.toggle();
+    this.triggerClick.emit(event);
+    // To handle the case where _the menu was just expanded by toggling the behavior subject_,
+    // hook into Angular's main change detection mechanism. When changes are stable
+    // (aka rendering is complete), focus the item
+    // See also: https://angular.io/guide/zone
+    this.ngZone.onStable
+      .pipe(take(1))
+      .subscribe(() =>
+        this.focusMenuItem(this.getSelectedMenuItemIndex() ?? 0)
+      );
+  }
+
+  handleItemClick() {
+    if (this.closeOnSelect) {
+      this.close();
+    }
+  }
+
+  toggle() {
+    if (this.isExpanded) {
+      this.close();
+    } else {
+      this.open();
+    }
+  }
+
+  close() {
+    this._isExpanded$.next(false);
+    this.triggerEl.nativeElement.focus();
+  }
+
+  open() {
+    this._isExpanded$.next(true);
+    this.ngZone.onStable.pipe(take(1)).subscribe(() => this.focusMenuItem(0));
+  }
+
+  findChildHandleFocus(ev: KeyboardEvent, menuItem: MenuItemDirective) {
+    const childIndex = this.menuItemEls
+      .toArray()
+      .findIndex((ref) => ref === menuItem);
+    this.handleMenuItemKeyDown(ev, childIndex);
+  }
+
+  onTriggerKeyUp(ev: KeyboardEvent): void {
+    const index = this.getSelectedMenuItemIndex() ?? 0;
     switch (ev.key) {
       case 'Up': // These first cases are IEEdge :(
       case 'ArrowUp':
@@ -174,15 +189,12 @@ export class MenuComponent implements OnInit, OnDestroy {
         break;
       case 'End':
       case 'ArrowEnd':
-        this.focusMenuItem(this.menuItems.length - 1);
+        this.focusMenuItem(this.menuItemEls.length - 1);
         break;
-      case 'Enter':
       case 'Space':
         this.focusMenuItem(index ?? 0);
         break;
       case 'Escape':
-        this.isExpanded$.next(false);
-        this.triggerEl.nativeElement.focus();
         break;
       default:
         return;
@@ -190,8 +202,7 @@ export class MenuComponent implements OnInit, OnDestroy {
     ev.preventDefault();
   }
 
-  onMenuItemKeyDown(ev: KeyboardEvent, index: number): void {
-    const selectedItem = this.menuItems[index] as MenuItem | undefined;
+  private handleMenuItemKeyDown(ev: KeyboardEvent, index: number): void {
     switch (ev.key) {
       case 'Up': // These first cases are IEEdge :(
       case 'ArrowUp':
@@ -207,22 +218,11 @@ export class MenuComponent implements OnInit, OnDestroy {
         break;
       case 'End':
       case 'ArrowEnd':
-        this.focusMenuItem(this.menuItems.length - 1);
+        this.focusMenuItem(this.menuItemEls.length - 1);
         break;
       case 'Enter':
       case 'Space':
         this.focusMenuItem(index);
-        if (selectedItem) {
-          this.handleItemClick(selectedItem, index);
-        }
-        break;
-      case 'Escape':
-        this.isExpanded$.next(false);
-        this.triggerEl.nativeElement.focus();
-        break;
-      case 'Tab':
-        // Trap tabs by breaking, and allowing `ev.preventDefault` to be called.
-        // The menu is in an open state and focus is trapped.
         break;
       default:
         return;
@@ -237,17 +237,19 @@ export class MenuComponent implements OnInit, OnDestroy {
     } else if (direction === 'desc') {
       i -= 1;
     }
+
+    const items = this.menuItemEls.toArray();
+
     // Circular navigation
     //
     // If we've went beyond "start" circle around to last
     if (i < 0) {
-      i = this.menuItems.length - 1;
-    } else if (i >= this.menuItems.length) {
+      i = items.length - 1;
+    } else if (i >= items.length) {
       // We've went beyond "last" so circle around to first
       i = 0;
     }
 
-    const items = this.menuItemEls.toArray();
     const nextMenuItemRef = items[i];
     const nextMenu = nextMenuItemRef ? nextMenuItemRef : null;
 
@@ -268,10 +270,19 @@ export class MenuComponent implements OnInit, OnDestroy {
     if (e == null) {
       return;
     }
-    if (this.isExpanded$.getValue() && this.closeOnClickOutside) {
+    if (this._isExpanded$.getValue() && this.closeOnClickOutside) {
       if (!this.isInside(e.target as HTMLElement)) {
-        this.isExpanded$.next(false);
-        this.triggerEl.nativeElement.focus();
+        this.close();
+      }
+    }
+
+    if (this._isExpanded$.getValue() && this.closeOnClickOutside === false) {
+      const targetIsMenuItem = this.menuItemEls
+        .map((menu) => menu.el.nativeElement)
+        .includes(e.target as HTMLButtonElement);
+      if (!targetIsMenuItem) {
+        // Trap focus when a click occurs outside, so that the user must interact with the menu
+        this.focusMenuItem(this.getSelectedMenuItemIndex() ?? 0);
       }
     }
   }
@@ -286,5 +297,10 @@ export class MenuComponent implements OnInit, OnDestroy {
       }
     }
     return false;
+  }
+
+  private getSelectedMenuItemIndex(): number | null {
+    const idx = this.menuItemEls.toArray().findIndex((ref) => ref.isSelected);
+    return idx > -1 ? idx : null;
   }
 }
