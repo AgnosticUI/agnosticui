fe3c3fecae632c0fc23c2870793e47ef03426684
Rob Levin <roblevintennis@gmail.com>
2022-04-11 10:27:31 -0500

    Adds eslint to the signup form playground so husky lint-staged passes lol

---
+++ playgrounds/SignupForm/src/components/React.jsx
@@ -0,0 +1,14 @@
+import "agnostic-react/dist/esm/index.css";
+import { Menu, MenuItem, } from "agnostic-react";
+
+export default function ReactAlert() {
+  return (
+    <section className="mbe40">
+      <div className="h4 mbe32 flex items-center">
+        <img width="24" height="24" src="/assets/React-icon.svg" alt="React logo" className="mie12" />React Close
+      </div>
+      <p className="mbs24 mbe14">Default Menu</p>
+      
+    </section>
+  );
+}
---
+++ playgrounds/SignupForm/src/components/Vue.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="mbe40">
+    <div class="h4 mbe32 flex items-center">
+      <img
+        width="24"
+        height="24"
+        src="/assets/Vue-icon.svg"
+        alt="Vue logo"
+        class="mie12"
+      />Vue 3
+    </div>
+  </section>
+</template>
+
+<script setup>
+// Components CSS
+import "agnostic-vue/dist/index.css";
+import { Menu } from "agnostic-vue";
+
+const log = (message, extra) => {
+  console.log(message, extra);
+};
+</script>
