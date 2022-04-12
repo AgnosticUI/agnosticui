8286b7683d3b28882dca823ee9016b4321d45393
Jordan LaPrise <officialtoxicdub@gmail.com>
2022-04-12 10:54:44 -0800

    add additional storybook tests

--- agnostic-svelte/src/lib/components/Table/Table.stories.js
+++ agnostic-svelte/src/lib/components/Table/Table.stories.js
@@ -138,3 +138,9 @@ CaptionBottom.args = { ...defaultArgs, captionPosition: "bottom" };
 
 export const CaptionEnd = Template.bind({});
 CaptionEnd.args = { ...defaultArgs, captionPosition: "end" };
+
+export const LimitFive = Template.bind({});
+LimitFive.args = { ...defaultArgs, limit: 5 };
+
+export const OffsetFive = Template.bind({});
+OffsetFive.args = { ...defaultArgs, offset: 5 };
