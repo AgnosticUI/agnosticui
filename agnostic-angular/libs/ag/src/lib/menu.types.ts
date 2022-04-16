7a418a6ff149336d18d2130d71300ec68bd7a4ee
Andrew Vitale <vitale232@gmail.com>
2022-04-16 08:38:36 -0400

    chore: cleanup and docs

--- agnostic-angular/libs/ag/src/lib/menu.types.ts
+++ agnostic-angular/libs/ag/src/lib/menu.types.ts
@@ -1,22 +1,34 @@
-import {
-  ElementRef,
-  EventEmitter,
-  InjectionToken,
-  NgZone,
-  QueryList,
-} from '@angular/core';
+import { EventEmitter, InjectionToken } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { MenuContentDirective } from './menu-content.directive';
 import { MenuItemDirective } from './menu-item.directive';
 
-export interface MenuItem {
-  label: string;
-  isDisabled?: boolean;
-}
+// Based on: https://github.com/storybookjs/storybook/issues/15732
 
 /** An Injection Token that can be used to inject a reference to the `MenuComponent` from child elements */
 export const AG_MENU = new InjectionToken<MenuBase>('AG_MENU');
 
+/**
+ * An interface used to represent the Agnostic UI `MenuComponent` in the Angular Dependency Injection system.
+ * By using a type in lieu of the component, we avoid circular dependencies.
+ *
+ * Example usage:
+ * ```ts
+ * // Parent Component:
+ * @Component({
+ *   providers: [{ provide: AG_MENU, useExisting: MenuComponent }],
+ * })
+ * export class MenuComponent {}
+ *
+ * // Child Component:
+ * @Directive({
+ *   selector: "button[ag-menu-item]",
+ * })
+ * export class MenuItemDirective {
+ *   constructor(@Inject(AG_MENU) private menu: MenuBase) {}
+ * }
+ * ```
+ */
 export interface MenuBase {
   closeOnSelect: boolean;
   closeOnClickOutside: boolean;
@@ -27,14 +39,9 @@ export interface MenuBase {
   menuTitle: string;
   size: 'small' | 'large' | undefined | null | '';
   triggerClick: EventEmitter<MouseEvent>;
-  selectionChange: EventEmitter<MenuItem>;
-  triggerEl: ElementRef<HTMLButtonElement>;
   content: MenuContentDirective;
-  menuItemEls: QueryList<MenuItemDirective>;
   _isExpanded$: BehaviorSubject<boolean>;
   readonly isExpanded: boolean;
-  ngZone: NgZone;
-  hostEl: ElementRef;
   onTriggerClick(event: MouseEvent): void;
   handleItemClick(): void;
   toggle(): void;
