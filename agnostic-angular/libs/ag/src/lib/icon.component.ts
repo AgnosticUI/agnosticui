31fd822e4be3af99668609242d2ed3c2aab4bc9a
Rob Levin <roblevintennis@gmail.com>
2022-04-22 12:59:03 -0500

    Refactors Angular Icon component to no longer use the positional > svg.

--- agnostic-angular/libs/ag/src/lib/icon.component.ts
+++ agnostic-angular/libs/ag/src/lib/icon.component.ts
@@ -1,19 +1,27 @@
-import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
+import { ElementRef, ChangeDetectionStrategy, Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'ag-icon',
   template: `<span [class]="iconClasses"><ng-content></ng-content></span>`,
-  styleUrls: ['./icon.css', './icon.host.css'],
+  styleUrls: ['./icon.css', 'icon-svg.css', './icon.host.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   // We receive SVG as Slot. But, we need width CSS applied to the SVG itself
-  // in Safari; so we need to enable global CSS e.g. `.icon-24 > svg { width: 24px }`
+  // in Safari; so we need to enable global CSS e.g. `.icon-24 > svg.icon-svg-24`
   encapsulation: ViewEncapsulation.None,
 })
-export class IconComponent {
+export class IconComponent implements OnInit {
   @Input() size?: 14 | 16 | 18 | 20 | 24 | 32 | 36 | 40 | 48 | 56 | 64 = 18;
   @Input() type: 'warning' | 'error' | 'success' | 'info' | '' = '';
   @Input() isSkinned?: boolean = true;
 
+  constructor(public $element: ElementRef<HTMLSpanElement>) { }
+
+  ngOnInit() {
+    const svg = this.$element.nativeElement.querySelector('svg');
+    svg?.classList.add('icon-svg');
+    if (this.size) svg?.classList.add(`icon-svg-${this.size}`);
+    if (this.type) svg?.classList.add(`icon-svg-${this.type}`);
+  }
   get iconClasses() {
     return [
       'icon',
