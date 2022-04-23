c76589d4ad9a4195df5dde6f2e1b22237aebd114
Gergely Csécsey <gcsecsey@gmail.com>
2022-04-23 21:13:14 +0200

    add storybook to project
    run `npx sb init` to bootstrap a storybook example in the agnostic-svelte-ts subproject

---
+++ agnostic-svelte-ts/.storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-svelte-csf"
+  ],
+  "framework": "@storybook/svelte",
+  "svelteOptions": {
+    "preprocess": require("../svelte.config.js").preprocess
+  }
+}
