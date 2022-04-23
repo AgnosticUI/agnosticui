74d79f07e32d249780ba9ff85259f5272caa873f
Gergely Csécsey <gcsecsey@gmail.com>
2022-04-23 21:17:33 +0200

    update storybook config type to commonjs

--- agnostic-svelte-ts/.storybook/main.js
+++ agnostic-svelte-ts/.storybook/main.js
@@ -1,15 +1,8 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-svelte-csf"
-  ],
-  "framework": "@storybook/svelte",
-  "svelteOptions": {
-    "preprocess": require("../svelte.config.js").preprocess
-  }
-}
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
+	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-svelte-csf'],
+	framework: '@storybook/svelte',
+	svelteOptions: {
+		preprocess: import('../svelte.config.js').preprocess
+	}
+};
