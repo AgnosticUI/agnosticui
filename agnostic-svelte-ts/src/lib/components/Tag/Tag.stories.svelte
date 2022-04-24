d6827296a2d183c35d7cc650f09cc17c2721322e
Rob Levin <roblevintennis@gmail.com>
2022-04-24 06:29:06 -0500

    Ports over Svelte Tags to typescript

---
+++ agnostic-svelte-ts/src/lib/components/Tag/Tag.stories.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Tag from './Tag.svelte';
+  import Tags from "./TagSlots.svelte";
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+</script>
+
+<Meta title="AG—Svelte (Beta)/Tags" component="{Tag}" />
+
+<Template let:args>
+	<Tags {...args} />
+</Template>
+
+<Story name="All" />
