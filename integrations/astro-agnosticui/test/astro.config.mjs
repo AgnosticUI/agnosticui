e619cb30bc4881d3fd89a611645e6348c7ce0350
Rob Levin <roblevintennis@gmail.com>
2022-04-29 05:38:35 -0500

    More misc astro-agnosticui integration tweaks

--- integrations/astro-agnosticui/test/astro.config.mjs
+++ integrations/astro-agnosticui/test/astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import agnosticAstro from '@astrojs/agnostic-astro';
+import agnosticAstro from 'astro-agnosticui';
 
 export default defineConfig({
   integrations: [agnosticAstro()]
