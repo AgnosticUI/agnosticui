3c00cf11cd57832ca9acc5a1441f736ada57c003
Rob Levin <roblevintennis@gmail.com>
2022-05-03 06:14:20 -0500

    Fix some errors in the Buttons playground and fix a11y contrast issue too.

--- playgrounds/Buttons/src/components/React.jsx
+++ playgrounds/Buttons/src/components/React.jsx
@@ -8,31 +8,31 @@ export default function React() {
       <div className="h4 mbe32 flex items-center">
         <img width="24" height="24" src="/assets/React-icon.svg" alt="React logo" className="mie12" />React Alerts
       </div>
-      <div class="mbe24 flex items-center">
+      <div className="mbe24 flex items-center">
         <Button css="mie8">Default</Button>
         <Button isCircle>Circ</Button>
       </div>
-      <div class="mbe24 flex items-center">
+      <div className="mbe24 flex items-center">
         <Button css="mie8" mode="primary">Primary</Button>
         <Button css="mie8" mode="primary" size="small">Small</Button>
       </div>
-      <div class="mbe24 flex items-center">
+      <div className="mbe24 flex items-center">
         <Button css="mie8" mode="primary" size="large">Large</Button>
         <Button css="mie8" mode="primary" isRounded>Rounded</Button>
       </div>
-      <div class="mbe24 flex items-center">
+      <div className="mbe24 flex items-center">
         <Button css="mie8" mode="secondary">Secondary</Button>
         <Button css="mie8" mode="secondary" isRounded>Secondary</Button>
       </div>
-      <div class="mbe24 flex items-center">
+      <div className="mbe24 flex items-center">
         <Button css="mie8" mode="primary" isBordered>Bordered</Button>
         <Button mode="primary" isBlock>Block</Button>
       </div>
-      <div class="mbe24 flex items-center">
+      <div className="mbe24 flex items-center">
         <Button css="mie8" mode="primary" isDisabled>Disabled</Button>
         <Button css="mie8" type="faux" mode="primary">Faux Button (div)</Button>
       </div>
-      <div class="mbe24 flex items-center">
+      <div className="mbe24 flex items-center">
         <Button css="mie8" mode="primary" isBlank>Blank</Button>
         <Button css="mie8" mode="primary" isLink>Button Link</Button>
         <Button css="mie8" isSkinned={false}>Unskinned</Button>
