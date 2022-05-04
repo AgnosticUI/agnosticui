2816c6e91f918a979988c63beb0a2a7f24a67906
Rob Levin <roblevintennis@gmail.com>
2022-05-03 20:21:57 -0500

    Adds Avatar component to svelte-ts

---
+++ agnostic-svelte-ts/src/lib/components/Avatar/Avatar.stories.svelte
@@ -0,0 +1,72 @@
+<script>
+import Avatar from "./Avatar.svelte";
+import FontDecorator from "../../storybook-helpers/FontDecorator.svelte";
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+</script>
+
+<Meta title="AG—Svelte (Beta)/Avatar" component="{Avatar}" />
+
+<Template let:args>
+  <Avatar {...args} />
+</Template>
+
+<Story name="Default" args="{{
+  text: 'AB'
+}}" />
+
+<Story name="Warning" args="{{
+  text: 'AB',
+  type: 'warning'
+}}" />
+
+<Story name="Info" args="{{
+  text: 'AB',
+  type: 'info'
+}}" />
+
+<Story name="Success" args="{{
+  text: 'AB',
+  type: 'success'
+}}" />
+
+<Story name="Error" args="{{
+  text: 'AB',
+  type: 'error'
+}}" />
+
+<Story name="Small" args="{{
+  size: 'small',
+  text: 'AB',
+  type: 'error',
+}}" />
+
+<Story name="Large" args="{{
+  size: 'large',
+  text: 'AB',
+  type: 'error',
+}}" />
+
+<Story name="XLarge" args="{{
+  size: 'xlarge',
+  text: 'AB',
+  type: 'error',
+}}" />
+
+<Story name="ImageUrl" args="{{
+  imgUrl: "https://joeschmoe.io/api/v1/random",
+}}" />
+
+<Story name="ImageWithTransparentBackground" args="{{
+  imgUrl: "https://joeschmoe.io/api/v1/random",
+  isTransparent: true,
+}}" />
+
+<Story name="ImageSquare" args="{{
+  imgUrl: "https://joeschmoe.io/api/v1/random",
+  isSquare: true,
+}}" />
+
+<Story name="ImageRoundedCorners" args="{{
+  imgUrl: "https://joeschmoe.io/api/v1/random",
+  isRounded: true,
+}}" />
---
+++ agnostic-svelte-ts/src/lib/components/Avatar/AvatarGroupSlots.svelte
@@ -0,0 +1,27 @@
+<style>
+  div {
+    margin-block-end: 1rem;
+  }
+</style>
+
+<script>
+  import Avatar from "./Avatar.svelte";
+  import AvatarGroup from "./AvatarGroup.svelte";
+</script>
+
+<div>
+  <AvatarGroup>
+    <Avatar text="AB" />
+    <Avatar text="CD" />
+    <Avatar text="XY" />
+  </AvatarGroup>
+</div>
+
+<div>
+  <AvatarGroup>
+    <Avatar text="S" type="success" />
+    <Avatar text="E" type="error" />
+    <Avatar text="W" type="warning" />
+    <Avatar text="I" type="info" />
+  </AvatarGroup>
+</div>
