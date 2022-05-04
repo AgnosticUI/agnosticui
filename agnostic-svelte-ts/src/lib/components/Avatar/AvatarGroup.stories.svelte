57fe6e8c59d463accf0cf0e96675236746eb3d9d
Rob Levin <roblevintennis@gmail.com>
2022-05-03 20:21:57 -0500

    Adds Avatar component to svelte-ts

---
+++ agnostic-svelte-ts/src/lib/components/Avatar/AvatarGroup.stories.svelte
@@ -0,0 +1,12 @@
+<script>
+import AvatarGroups from "./AvatarGroupSlots.svelte";
+import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+</script>
+
+<Meta title="AG—Svelte (Beta)/AvatarGroups" component="{AvatarGroups}" />
+
+<Template let:args>
+	<AvatarGroups {...args} />
+</Template>
+
+<Story name="Examples" />
---
+++ agnostic-svelte-ts/src/lib/components/Avatar/AvatarWithSvg.stories.svelte
@@ -0,0 +1,12 @@
+<script>
+import AvatarSvgExample from "./AvatarSvgExample.svelte";
+import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+</script>
+
+<Meta title="AG—Svelte (Beta)/Avatar with SVG" component="{AvatarSvgExample}" />
+
+<Template let:args>
+  <AvatarSvgExample {...args} />
+</Template>
+
+<Story name="Examples" />
