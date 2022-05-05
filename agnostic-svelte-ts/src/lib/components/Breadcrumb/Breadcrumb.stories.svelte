0c36f39ab0d29f0cbc502e8e66c3b5e82ab9f357
Rob Levin <roblevintennis@gmail.com>
2022-05-04 21:56:04 -0500

    Updates svelte-ts copystyles to actually copy over the styles for Alert and Avatar; and now Breadcrumb too which was also ported to TS

---
+++ agnostic-svelte-ts/src/lib/components/Breadcrumb/Breadcrumb.stories.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+import Breadcrumb from "./Breadcrumb.svelte";
+import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+const tennisRoutes = [
+  {
+    label: "Tennis",
+    url: "#tennis",
+  },
+  {
+    label: "Superstars",
+    url: "#tennis-superstars",
+  },
+  {
+    label: "Serena Williams",
+    url: "#tennis-superstars-serena",
+  },
+];
+</script>
+
+<Meta title="AG—Svelte (Beta)/Breadcrumb" component="{Breadcrumb}" />
+
+<Template let:args>
+	<Breadcrumb {...args} />
+</Template>
+
+<Story name="Default" args="{{ routes: tennisRoutes }}" />
+
+<Story name="Slash" args="{{ routes: tennisRoutes, type: 'slash' }}" />
+
+<Story name="Arrow" args="{{ routes: tennisRoutes, type: 'arrow' }}" />
+
+<Story name="Bullet" args="{{ routes: tennisRoutes, type: 'bullet' }}" />
+
+<Story name="Single" args="{{ routes: [{ label: 'A single route will look *linkless*' }] }}" />
+
+<Story name="TwoRoutes" args="{{ 
+  routes: [
+    { label: "one", url: "#one" },
+    { label: "two", url: "#two" },
+  ]
+}}" />
