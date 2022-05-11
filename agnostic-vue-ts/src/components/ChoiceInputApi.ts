a075550dcd24d801ae7cdb79eb01504e1a2da985
Rob Levin <roblevintennis@gmail.com>
2022-05-10 19:52:24 -0500

    Pushes the vue typescript implementation over into agnostic-vue

--- agnostic-vue-ts/src/components/ChoiceInputApi.ts
+++
@@ -1,7 +0,0 @@
-export type ChoiceInputTypes = "checkbox" | "radio";
-export interface ChoiceInputOptions {
-  name: string;
-  value: string;
-  label: string;
-}
-export type ChoiceInputSizes = "small" | "large" | "";
