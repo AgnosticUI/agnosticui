2be5cbd88e8260f754ede4ff1f55b1014ae46677
Rob Levin <roblevintennis@gmail.com>
2022-05-13 18:49:20 -0500

    Adds progress component to svelte typescript port

---
+++ agnostic-svelte-ts/src/lib/components/Progress/Progress.stories.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+import Progress from "./Progress.svelte";
+import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+</script>
+
+<Meta title="AG—Svelte (Beta)/Progress" component="{Progress}" />
+<Template let:args>
+  <Progress {...args} />
+</Template>
+
+<Story name="ProgressTest" args="{{
+  value: 30,
+  max: 100,
+}}" />
+
+<Story name="ProgressCSS" args="{{
+  value: 30,
+  max: 100,
+  css: "foo-bar-baz",
+}}" />
