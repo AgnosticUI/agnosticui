c0552b4d7d9183fbc3ba983824c9ffa70d5aa072
Rob Levin <roblevintennis@gmail.com>
2022-05-14 16:53:13 -0500

    agnostic-svelte-ts port -- Take 2 -- adds Input and InputAddon component.

--- agnostic-svelte-ts/package_test/src/routes/index.svelte
+++ agnostic-svelte-ts/package_test/src/routes/index.svelte
@@ -23,8 +23,8 @@
 		HeaderNav,
 		HeaderNavItem,
 		Icon,
-		// Input,
-		// InputAddonItem,
+		Input,
+		InputAddonItem,
 		// Loader,
 		// Menu,
 		// MenuItem,
@@ -815,6 +815,59 @@
         </Icon>
       </div>
     </section>
+    <Card>
+      <div class="h4 w-100">Input</div>
+      <div><code>bind:value</code> test: {valueText}</div>
+      <Input bind:value={valueText} placeholder="type in here to verify bind:value" />
+      <Input id="1" label="Default input" />
+      <Input id="2" isRounded label="Rounded input" />
+      <Input id="3" isUnderlined label="Underlined input" />
+      <Input id="4" isUnderlined isUnderlinedWithBackground label="Underlined with background" />
+      <Input id="5" isInline label="Inline input" />
+      <Input id="6" size="small" label="Small input" />
+      <Input id="7" size="large" label="Large input" />
+      <button class="mie32" on:click={() => testHelpText=!testHelpText}>Toggle help text</button>
+      <Input id="8" helpText={testHelpText ? 'Some useful help hint…' : null} label="Help text" />
+      <button class="mie32" on:click={() => testIsInvalid=!testIsInvalid}>Toggle is invalid</button>
+      <Input id="9" isInvalid={testIsInvalid} invalidText="Some error hint…" label="Error hints" />
+      <div><code>bind:value</code> when using input addons: {addonValueText}</div>
+      <Input
+        bind:value={addonValueText}
+        placeholder="type in here to verify bind:value"
+        hasLeftAddon={true}
+        hasRightAddon={true}
+        id="10"
+        label="Input with addons"
+      >
+        <div slot="addonLeft">
+          <InputAddonItem addonLeft={true}><span>L</span></InputAddonItem>
+        </div>
+        <div slot="addonRight">
+          <InputAddonItem addonRight={true}><span>R</span></InputAddonItem>
+        </div>
+      </Input>
+      <p class="mbs24">Dynamic Input (#114)</p>
+      <Input
+        hasRightAddon={true}
+        id="bug114"
+        type={textIsVisible ? 'text' : 'password'}
+        label="Password input toggle (visible / invisible)"
+      >
+        <div slot="addonRight">
+          <InputAddonItem addonRight={true}>
+            <Button isBlank on:click={toggleTextVisibility}>
+              {#if textIsVisible}
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--ion" width="1.125rem" height="1.125rem" preserveAspectRatio="xMidYMid meet" viewBox="0 0 512 512" data-icon="ion:md-eye-off"><path d="M256.1 144.8c56.2 0 101.9 45.3 101.9 101.1 0 13.1-2.6 25.5-7.3 37l59.5 59c30.8-25.5 55-58.4 69.9-96-35.3-88.7-122.3-151.6-224.2-151.6-28.5 0-55.8 5.1-81.1 14.1l44 43.7c11.6-4.6 24.1-7.3 37.3-7.3zM52.4 89.7l46.5 46.1 9.4 9.3c-33.9 26-60.4 60.8-76.3 100.8 35.2 88.7 122.2 151.6 224.1 151.6 31.6 0 61.7-6.1 89.2-17l8.6 8.5 59.7 59 25.9-25.7L78.2 64 52.4 89.7zM165 201.4l31.6 31.3c-1 4.2-1.6 8.7-1.6 13.1 0 33.5 27.3 60.6 61.1 60.6 4.5 0 9-.6 13.2-1.6l31.6 31.3c-13.6 6.7-28.7 10.7-44.8 10.7-56.2 0-101.9-45.3-101.9-101.1 0-15.8 4.1-30.7 10.8-44.3zm87.8-15.7l64.2 63.7.4-3.2c0-33.5-27.3-60.6-61.1-60.6l-3.5.1z" fill="currentColor"></path></svg>
+              {:else}
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--ion" width="1.125rem" height="1.125rem" preserveAspectRatio="xMidYMid meet" viewBox="0 0 512 512" data-icon="ion:md-eye"><path d="M256 105c-101.8 0-188.4 62.4-224 151 35.6 88.6 122.2 151 224 151s188.4-62.4 224-151c-35.6-88.6-122.2-151-224-151zm0 251.7c-56 0-101.8-45.3-101.8-100.7S200 155.3 256 155.3 357.8 200.6 357.8 256 312 356.7 256 356.7zm0-161.1c-33.6 0-61.1 27.2-61.1 60.4s27.5 60.4 61.1 60.4 61.1-27.2 61.1-60.4-27.5-60.4-61.1-60.4z" fill="currentColor"></path></svg>
+              {/if}
+            </Button>
+          </InputAddonItem>
+        </div>
+      </Input>
+      <div><code>bind:value</code> when using textarea: {textareaValueText}</div> 
+      <Input type='textarea' bind:value={textareaValueText} placeholder="Textarea works with bind:value too!"></Input>
+    </Card>
     <section class="mbe24">
       <EmptyState>
         <div slot="header">
