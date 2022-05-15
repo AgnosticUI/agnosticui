9cef75d6a72948f10ce5cb7b144d8ea04b12c7f0
Rob Levin <roblevintennis@gmail.com>
2022-05-14 21:57:16 -0500

    agnostic-svelte-ts port -- adds Select component.

---
+++ agnostic-svelte-ts/src/lib/components/Select/Select.stories.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+  import Select from "./Select.svelte";
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+  import { action } from "@storybook/addon-actions";
+
+  const actionsData = {
+    selected: action("selected"),
+  };
+
+  const tennisOptions = [
+    { value: "andre", label: "Andre Agassi" },
+    { value: "serena", label: "Serena Williams" },
+    { value: "mac", label: "John McEnroe" },
+    { value: "borg", label: "Bjorn Borg" },
+    { value: "althea", label: "Althea Gibson" },
+    { value: "roger", label: "Roger Federer" },
+  ];
+</script>
+  
+<Meta title="AG—Svelte (Beta)/Select" component="{Select}" />
+<Template let:args>
+  <Select
+    {...args} 
+    selected={actionsData.selected}
+  />
+</Template>
+
+<Story name="Default" args="{{
+  uniqueId: "sel1",
+  name: "select-1",
+  labelCopy: "this is label copy",
+  options: tennisOptions,
+}}" />
+
+<Story name="FirstOptionLabel" args="{{
+  defaultOptionLabel: "Select your favorite tennis player of all-time",
+  uniqueId: "sel2",
+  name: "select-2",
+  labelCopy: "this is label copy",
+  options: tennisOptions,
+}}" />
+
+<Story name="Disabled" args="{{
+  uniqueId: "sel3",
+  name: "select-3",
+  isDisabled: true,
+  labelCopy: "this is label copy",
+  options: tennisOptions,
+}}" />
+
+<Story name="Small" args="{{
+  uniqueId: "sel4",
+  name: "select-4",
+  size: "small",
+  labelCopy: "this is label copy",
+  options: tennisOptions,
+}}" />
+
+<Story name="Large" args="{{
+  uniqueId: "sel5",
+  name: "select-5",
+  size: "large",
+  labelCopy: "this is label copy",
+  options: tennisOptions,
+}}" />
+
+<Story name="Multiple" args="{{
+  uniqueId: "sel6",
+  name: "select-6",
+  isMultiple: true,
+  multipleSize: 5,
+  labelCopy: "this is label copy",
+  options: tennisOptions,
+}}" />
---
+++ agnostic-svelte-ts/src/lib/components/Select/Select.svelte
@@ -0,0 +1,157 @@
+<style>
+.select,
+.select-base {
+  display: block;
+  width: 100%;
+  /* stylelint-disable-next-line property-no-vendor-prefix */
+  -webkit-appearance: none;
+  /* stylelint-disable-next-line property-no-vendor-prefix */
+  -moz-appearance: none;
+  appearance: none;
+}
+
+.select,
+.select-skin {
+  padding: var(--fluid-6) var(--fluid-32) var(--fluid-6) var(--fluid-12);
+  -moz-padding-start: calc(var(--fluid-12) - 3px);
+  font-size: var(--fluid-16);
+  font-weight: 400;
+  line-height: 1.5;
+  color: var(--agnostic-dark);
+  border: 1px solid var(--agnostic-select-border-color, var(--agnostic-gray-light));
+  border-radius: var(--agnostic-radius);
+  transition:
+    border-color var(--agnostic-timing-fast) ease-in-out,
+    box-shadow var(--agnostic-timing-fast) ease-in-out;
+}
+
+/* Only shows the down arrow SVG if in single mode */
+.select:not([multiple]) {
+  background-color: inherit;
+  background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke='%23333330' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M2 5l6 6 6-6'/%3e%3c/svg%3e");
+  background-repeat: no-repeat;
+  background-position: right var(--fluid-12) center;
+  background-size: var(--fluid-16) var(--fluid-12);
+}
+
+.select:focus {
+  border-color: var(--agnostic-focus-ring-color);
+  box-shadow: 0 0 0 var(--agnostic-focus-ring-outline-width) var(--agnostic-focus-ring-color);
+
+  /* Needed for High Contrast mode */
+  outline:
+    var(--agnostic-focus-ring-outline-width) var(--agnostic-focus-ring-outline-style)
+    var(--agnostic-focus-ring-outline-color);
+  transition: box-shadow var(--agnostic-timing-fast) ease-out;
+}
+
+.select-base,
+.select:disabled {
+  background-color: var(--agnostic-disabled-bg);
+}
+
+.select-base,
+.select:-moz-focusring {
+  color: transparent;
+  text-shadow: 0 0 0 var(--agnostic-dark);
+}
+
+@media (prefers-reduced-motion), (update: slow) {
+  .select,
+  .select-base,
+  .select:focus {
+    transition: none;
+  }
+}
+
+.select-small {
+  padding-top: var(--fluid-4);
+  padding-bottom: var(--fluid-4);
+  padding-left: var(--fluid-8);
+  font-size: var(--fluid-14);
+}
+
+.select-large {
+  padding-top: var(--fluid-8);
+  padding-bottom: var(--fluid-8);
+  padding-left: var(--fluid-16);
+  font-size: var(--fluid-18);
+}
+
+</style>
+
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+  export let uniqueId = "";
+  export let name = "";
+  export let labelCopy = "";
+  export let options = [];
+  export let size: "small" | "large" | "" = "";
+  export let multipleSize = 1;
+  export let isMultiple = false;
+  export let defaultOptionLabel = "Please select an option";
+  export let isDisabled = false;
+  export let isSkinned = true;
+  export let css = "";
+
+  // selected can be used for two-way bindings
+  export let selected;
+
+  // If we don't make it seems Svelte gets confused:
+  // https://github.com/sveltejs/svelte/issues/5644
+  // And so we cannot share selected above :(
+  export let multiSelected = [];
+
+  const dispatch = createEventDispatcher();
+  // This will emit an event object that has a event.detail prop
+  // This will contain the value of the selected option value. See
+  // https://svelte.dev/docs#createEventDispatcher
+  const changeHandler = () => {
+    dispatch("selected", isMultiple ? multiSelected : selected);
+  };
+
+  $: disable = isDisabled;
+  $: classes = [
+    isSkinned ? "select" : "select-base",
+    size ? `select-${size}` : "",
+    css ? `${css}` : "",
+  ]
+    .filter((cl) => cl)
+    .join(" ");
+
+</script>
+
+<label class="screenreader-only" for={uniqueId}> {labelCopy} </label>
+{#if isMultiple}
+  <select
+    id={uniqueId}
+    class={classes}
+    name={name}
+    disabled={disable}
+    multiple
+    size={multipleSize}
+    bind:value={multiSelected}
+    on:change={changeHandler}
+  >
+    {#each options as { value, label }}
+      <option value={value}>{label}</option>
+    {/each}
+  </select>
+{:else}
+  <select
+    id={uniqueId}
+    class={classes}
+    name={name}
+    disabled={disable}
+    bind:value={selected}
+    on:change={changeHandler}
+  >
+    <option value="select-option" disabled selected>
+      {defaultOptionLabel}
+    </option>
+    {#each options as { value, label }}
+      <option value={value}>{label}</option>
+    {/each}
+  </select>
+{/if}
