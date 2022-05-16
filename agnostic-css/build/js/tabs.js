69d4c8719169467e26f3030cc7966a734328f3d5
Rob Levin <roblevintennis@gmail.com>
2022-05-16 06:40:59 -0500

    Remove extraneous build directory and bump version for css package

--- agnostic-css/build/js/tabs.js
+++
@@ -1,8 +0,0 @@
-// https://github.com/agnosticui/a11y-tabs
-
-/**
- * This all depends on the CDN loaded a11y-tabs -- see _preview.hbs in src/components/tabs
- */
-window.addEventListener('DOMContentLoaded', () => {
-  new A11yTabs('.tab-list', '[role="tabpanel"]', 0)
-})
