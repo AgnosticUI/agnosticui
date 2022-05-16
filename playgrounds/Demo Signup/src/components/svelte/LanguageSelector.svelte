22a57c091ec78fdc8b08b434be771ae53826bbd2
Rob Levin <roblevintennis@gmail.com>
2022-05-16 07:02:39 -0500

    Adds Demo Signup playground

---
+++ playgrounds/Demo Signup/src/components/svelte/LanguageSelector.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  // import Select from 'agnostic-svelte';
+  import { Select } from "agnostic-svelte";
+  let selectedValue;
+  let multiSelectValue;
+  const handleChange = e => {
+    const name = e.target.getAttribute('name');
+    const value = e.target.value;
+    console.log('value: ', value)
+  };
+  const countryOptions = [
+    { value: 'ja', label: '日本語' },
+    { value: 'it', label: 'Italiano' },
+    { value: 'ko', label: '한국어' },
+    { value: 'en_US', label: 'English' },
+    { value: 'es', label: 'Español' },
+    { value: 'zh_CN', label: '简体中文' },
+    { value: 'th', label: 'ภาษาไทย' },
+    { value: 'sv', label: 'Svenska' },
+    { value: 'zh_TW', label: '繁體中文' },
+    { value: 'nl_NL', label: 'Nederlands' },
+    { value: 'da', label: 'Dansk' },
+    { value: 'no', label: 'Norsk' },
+    { value: 'ru', label: 'Русский' },
+    { value: 'fr', label: 'Français' },
+    { value: 'fi', label: 'Suomi' },
+    { value: 'de', label: 'Deutsch' },
+  ];
+</script>
+
+<section class="language">
+  <form on:submit|preventDefault>
+    <Select bind:selected={selectedValue} uniqueId="langSelector" name="languageselector" defaultOptionLabel="English" labelCopy="Please select a language" options={countryOptions} />
+  </form>
+</section>
+
+<style>
+
+  .h2 {
+    font-family: Cochin, Georgia, 'Times New Roman', serif;
+    letter-spacing: 0.03rem;
+  }
+
+  .password-eye-btn :global(svg) {
+    color: var(--neutral-x-dark);
+  }
+  .password-eye-btn {
+    opacity: 75%;
+    transition: opacity var(--agnostic-timing-medium);
+  }
+
+  .password-eye-btn:hover {
+    opacity: 100%;
+  }
+</style>
