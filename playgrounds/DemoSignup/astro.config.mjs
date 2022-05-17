02b179639cefc592a5f8291e00fa3808c3e5fa49
Rob Levin <roblevintennis@gmail.com>
2022-05-17 10:59:27 -0500

    Updates README. Removes React since we're not using. Cleanup

--- playgrounds/DemoSignup/astro.config.mjs
+++ playgrounds/DemoSignup/astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import svelte from '@astrojs/svelte';
-import react from '@astrojs/react';
 import agnosticAstro from 'agnosticui-astro';
 
 export default defineConfig({
-	integrations: [vue(), react(), svelte(), agnosticAstro()]
+	integrations: [vue(), svelte(), agnosticAstro()]
 });
