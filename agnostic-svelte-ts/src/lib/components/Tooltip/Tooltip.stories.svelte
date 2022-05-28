c481b1aa821ae80c1df668c1c3e54c6c30f47162
Rob Levin <roblevintennis@gmail.com>
2022-05-28 17:46:24 -0500

    Adds Tooltips to agnostic-svelte-ts

---
+++ agnostic-svelte-ts/src/lib/components/Tooltip/Tooltip.stories.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import TooltipSlots from "./TooltipSlots.svelte";
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+</script>
+<Meta title="AG—Svelte (Beta)/Tooltip" component="{TooltipSlots}" />
+
+<Template let:args>
+  <TooltipSlots {...args} />
+</Template>
+<Story name="Examples" args="{{}}" />
---
+++ agnostic-svelte-ts/src/lib/components/Tooltip/api.ts
@@ -0,0 +1,13 @@
+export type TooltipPlacement =
+  | "top"
+  | "top-start"
+  | "top-end"
+  | "right"
+  | "right-start"
+  | "right-end"
+  | "bottom"
+  | "bottom-start"
+  | "bottom-end"
+  | "left"
+  | "left-start"
+  | "left-end";
