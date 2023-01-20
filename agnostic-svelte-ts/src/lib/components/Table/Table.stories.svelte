bebdea8c21f2fe02ccba5227e450ad4bbcb1fa84
Rob Levin <roblevintennis@gmail.com>
2023-01-20 16:26:38 -0600

    Adds Table that compiles (WIP)

---
+++ agnostic-svelte-ts/src/lib/components/Table/Table.stories.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Table from "./Table.svelte";
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+  </script>
+  <Meta title="AG—Svelte (Beta)/Table" component="{Table}" />
+  
+  <Template let:args>
+    <Table
+      {...args}
+    />
+  </Template>
+  <Story name="Default" args="{{}}" />
+
+  <!--   
+  <Story name="Small" args="{{
+    size: "small"
+  }}" />
+  
+  <Story name="Large" args="{{
+    size: "large"
+  }}" />
+  
+  <Story name="XLarge" args="{{
+    size: "xlarge"
+  }}" />
+   -->
