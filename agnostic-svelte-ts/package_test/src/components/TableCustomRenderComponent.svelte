4285ca3c78afc04ddf52739e2d9da5ab21db9f32
Rob Levin <roblevintennis@gmail.com>
2023-01-23 17:07:22 -0600

    Adds Table to agnostic-svelte-ts package_test

---
+++ agnostic-svelte-ts/package_test/src/components/TableCustomRenderComponent.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  /**
+   * This is just an example that is used within the `renderComponent` in ./Table.stories.js
+   * `headers` like: { renderComponent: () => TableCustomRenderComponent },
+   *
+   * Also, it would have been nice to be able to generate this component "inline" but it seems
+   * there's an RFC for a long time but hasn't materialized:
+   * https://github.com/sveltejs/rfcs/blob/inline-components/text/0000-inline-components.md
+   */
+  export let cellValue: any;
+</script>
+
+<td>{cellValue}<span class="mis6">🏆</span></td>
