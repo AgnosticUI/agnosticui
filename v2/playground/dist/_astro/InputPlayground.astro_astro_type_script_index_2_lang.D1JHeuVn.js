dc794686eff1f916cb42f78c60b86ecf8d990fa4
Rob Levin <roblevintennis@gmail.com>
2025-09-21 22:40:14 -0500

    Fix Dialog naming consistency to use ag- prefix like other components
    - Change @customElement from 'agnostic-dialog' to 'ag-dialog'
    - Update playground HTML from <agnostic-dialog> to <ag-dialog>
    - Update playground scripts to wait for 'ag-dialog' custom element
    - Update test assertion to expect 'ag-dialog' tag name
    - Now consistent with ag-button, ag-input, ag-accordion naming pattern
    - All 54 Dialog tests still passing, library and playground build successfully

--- v2/playground/dist/_astro/InputPlayground.astro_astro_type_script_index_2_lang.D1JHeuVn.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["_astro/index.j-eLgrKq.js","_astro/lit-html-Bm_EwbrZ.B3KIfP8H.js","_astro/lit-element.DPA3KJ4y.js","_astro/MinimalAccordion.DAwcHJU4.js","_astro/_Input.bx2nze_F.js","_astro/_Tooltip.Dtj3g-9G.js"])))=>i.map(i=>d[i]);
-import{_}from"./preload-helper.BlTxHScW.js";_(()=>import("./index.j-eLgrKq.js"),__vite__mapDeps([0,1,2,3,4,5]));
