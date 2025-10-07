5caf6ba750f6b8150ebea49d6ed9f03a7c167419
Rob Levin <roblevinillustration@gmail.com>
2025-10-07 17:14:44 -0500

    Starting on Vue playground. Generates custom-elements.json (not really using yet)

---
+++ v2/playgrounds/vue/src/stories/Button.vue
@@ -0,0 +1,46 @@
+<template>
+  <VueButton
+    :type="'button'"
+    :variant="primary ? 'primary' : 'secondary'"
+    :size="size === 'small' ? 'sm' : size === 'large' ? 'lg' : 'md'"
+    @click="onClick"
+  >
+    {{ label }}
+  </VueButton>
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+import "agnosticui-core/button"; // Register the ag-button web component
+import VueButton from "agnosticui-core/button/vue"; // Import the compiled VueButton module
+
+const props = withDefaults(
+  defineProps<{
+    /**
+     * The label of the button
+     */
+    label: string;
+    /**
+     * Primary or secondary button
+     */
+    primary?: boolean;
+    /**
+     * Size of the button
+     */
+    size?: "small" | "medium" | "large";
+    /**
+     * Background color of the button (not supported by VueButton, included for compatibility)
+     */
+    backgroundColor?: string;
+  }>(),
+  { primary: false }
+);
+
+const emit = defineEmits<{
+  (e: "click", id: number): void;
+}>();
+
+const onClick = () => {
+  emit("click", 1);
+};
+</script>
