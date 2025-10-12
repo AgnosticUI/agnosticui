52d0a7ec81c8297d6f36e585c0ef4d35c2979eba
Rob Levin <roblevinillustration@gmail.com>
2025-10-11 22:45:04 -0500

    Adding custom theme and light/dark mode toggle synchronization. Adds a Button example.

--- v2/site/docs/.vitepress/theme/index.ts
+++ v2/site/docs/.vitepress/theme/index.ts
@@ -4,14 +4,12 @@ import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 import './custom.css'
+import MyLayout from './MyLayout.vue'
 
 export default {
   extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
+  Layout: MyLayout,
+
   enhanceApp({ app, router, siteData }) {
     // ...
   }
