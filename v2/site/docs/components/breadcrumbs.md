a7b0b81d316cfdc0b0343ae52baee7228fe612d2
Rob Levin <roblevinillustration@gmail.com>
2025-10-12 17:08:16 -0500

    Trying to spike Breadcrumb in Vitepress (not yet working)

--- v2/site/docs/components/breadcrumbs.md
+++ v2/site/docs/components/breadcrumbs.md
@@ -1,3 +1,20 @@
 # Breadcrumbs
 
 The `Breadcrumb` component is a <span class="quoted">wayfinding</span> element that helps make users aware of their current location within an application or website.
+
+## Examples
+
+<BreadcrumbExamples />
+
+<script setup>
+import BreadcrumbExamples from '../examples/BreadcrumbExamples.vue'
+</script>
+
+## Usage
+
+```vue{4}
+<template>
+the goodness
+</template>
+
+```
---
+++ v2/site/docs/examples/BreadcrumbExamples.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <VueBreadrumb :items="items" />
+  </section>
+</template>
+<script>
+import VueBreadcrumb from "agnosticui-core/breadcrumb/vue";
+export default {
+  name: "BreadcrumbExamples",
+  components: { VueBreadcrumb },
+  data() {
+    return {
+      items: [
+        { label: "Home", href: "/" },
+        { label: "About", href: "/about" },
+        { label: "Services", href: "/services" },
+        { label: "Products", href: "/products" },
+      ],
+    };
+  },
+};
+</script>
