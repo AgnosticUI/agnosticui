4c69b2a514d81abd8e338ff879ed0c7674fd81ce
Rob Levin <roblevinillustration@gmail.com>
2025-10-11 21:04:18 -0500

    Vitepress theming spike

--- v2/site/docs/.vitepress/theme/index.ts
+++ v2/site/docs/.vitepress/theme/index.ts
@@ -3,6 +3,7 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
+import './custom.css'
 
 export default {
   extends: DefaultTheme,
