a047f909b0781c0de85a39112a8379d71b6f7abb
Rob Levin <roblevintennis@gmail.com>
2025-10-25 15:23:17 -0500

    Avatar barrel

--- v2/lib/src/components/Avatar/core/Avatar.ts
+++ v2/lib/src/components/Avatar/core/Avatar.ts
@@ -1,6 +1,14 @@
-
-import { Avatar } from './_Avatar';
+import { Avatar } from './_Avatar.js';
 
 if (!customElements.get('ag-avatar')) {
   customElements.define('ag-avatar', Avatar);
 }
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'ag-avatar': Avatar;
+  }
+}
+
+// Export everything from implementation
+export * from './_Avatar.js';
