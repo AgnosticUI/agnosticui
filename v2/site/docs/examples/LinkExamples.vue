5f78c1bb6f05af0d46ac525943cca70b66c6335d
Rob Levin <roblevintennis@gmail.com>
2025-11-10 11:59:55 -0600

    Fixes  Link page links

--- v2/site/docs/examples/LinkExamples.vue
+++ v2/site/docs/examples/LinkExamples.vue
@@ -4,53 +4,87 @@
     <div class="mbe4">
       <h3>Basic Links</h3>
       <div class="stacked-mobile">
-        <VueLink href="/home">Home</VueLink>
-        <VueLink href="/about">About</VueLink>
+        <VueLink href="#link">Home</VueLink>
+        <VueLink href="#link">About</VueLink>
       </div>
     </div>
 
     <div class="mbe4">
       <h3>Variant Links</h3>
       <div class="stacked-mobile">
-        <VueLink href="/success" variant="success">Success Link</VueLink>
-        <VueLink href="/warning" variant="warning">Warning Link</VueLink>
-        <VueLink href="/danger" variant="danger">Danger Link</VueLink>
+        <VueLink
+          href="#link"
+          variant="success"
+        >Success Link</VueLink>
+        <VueLink
+          href="#link"
+          variant="warning"
+        >Warning Link</VueLink>
+        <VueLink
+          href="#link"
+          variant="danger"
+        >Danger Link</VueLink>
       </div>
     </div>
 
     <div class="mbe4">
       <h3>Button-Style Links</h3>
       <div class="stacked-mobile">
-        <VueLink href="/action" :isButton="true">Button Link</VueLink>
-        <VueLink href="/action" :isButton="true" variant="success">Success Button</VueLink>
-        <VueLink href="/action" :isButton="true" variant="warning">Warning Button</VueLink>
-        <VueLink href="/action" :isButton="true" variant="danger">Danger Button</VueLink>
+        <VueLink
+          href="#link"
+          :isButton="true"
+        >Button Link</VueLink>
+        <VueLink
+          href="#link"
+          :isButton="true"
+          variant="success"
+        >Success Button</VueLink>
+        <VueLink
+          href="#link"
+          :isButton="true"
+          variant="warning"
+        >Warning Button</VueLink>
+        <VueLink
+          href="#link"
+          :isButton="true"
+          variant="danger"
+        >Danger Button</VueLink>
       </div>
     </div>
 
     <div class="mbe4">
       <h3>External Link</h3>
       <div class="stacked-mobile">
-        <VueLink href="https://github.com" :external="true">External Link →</VueLink>
+        <VueLink
+          href="https://github.com"
+          :external="true"
+        >External Link →</VueLink>
       </div>
     </div>
 
     <div class="mbe4">
       <h3>Disabled Links</h3>
       <div class="stacked-mobile">
-        <VueLink href="/disabled" :disabled="true">Disabled Link</VueLink>
-        <VueLink href="/disabled" :disabled="true" :isButton="true">Disabled Button</VueLink>
+        <VueLink
+          href="#link"
+          :disabled="true"
+        >Disabled Link</VueLink>
+        <VueLink
+          href="#link"
+          :disabled="true"
+          :isButton="true"
+        >Disabled Button</VueLink>
       </div>
     </div>
   </section>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { VueLink } from 'agnosticui-core/link/vue';
+import { defineComponent } from "vue";
+import { VueLink } from "agnosticui-core/link/vue";
 
 export default defineComponent({
-  name: 'LinkExamples',
+  name: "LinkExamples",
   components: {
     VueLink,
   },
