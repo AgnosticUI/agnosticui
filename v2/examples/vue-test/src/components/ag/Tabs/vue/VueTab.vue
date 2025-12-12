2921d9e51fc6907fb011a9a7d075f9228e8d5b27
Rob Levin <roblevintennis@gmail.com>
2025-12-11 19:23:15 -0600

    Adds peerDependencies and rest of vue-test examples component

---
+++ v2/examples/vue-test/src/components/ag/Tabs/vue/VueTab.vue
@@ -0,0 +1,22 @@
+<template>
+  <ag-tab
+    slot="tab"
+    :panel="panel"
+    .disabled="disabled"
+    :aria-disabled="disabled ? 'true' : null"
+    v-bind="$attrs"
+  >
+    <slot />
+  </ag-tab>
+</template>
+
+<script setup lang="ts">
+// Define props interface
+export interface VueTabProps {
+  panel: string;
+  disabled?: boolean;
+}
+
+// Define props
+defineProps<VueTabProps>();
+</script>
