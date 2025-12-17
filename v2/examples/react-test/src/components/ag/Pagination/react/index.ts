9a286e814bcb9d99549e453879a00bfa81875de8
Rob Levin <roblevintennis@gmail.com>
2025-12-17 12:06:48 -0500

    Adds Dialog, Divider, Drawer, EmptyState, MessageBubble, Pagination, Toast, Toggle, Tooltip, VisuallyHidden

---
+++ v2/examples/react-test/src/components/ag/Pagination/react/index.ts
@@ -0,0 +1,15 @@
+export { ReactPagination } from './ReactPagination';
+export type { ReactPaginationProps } from './ReactPagination';
+
+// Re-export types from core for convenience
+export type { PageChangeEventDetail, PageChangeEvent } from '../core/Pagination';
+
+export { ReactContentPagination } from './ReactContentPagination';
+export type { ReactContentPaginationProps } from './ReactContentPagination';
+
+// Re-export types from core for convenience
+export type {
+  NavigateEventDetail,
+  NavigateEvent,
+  ContentNavigationItem,
+} from '../core/ContentPagination';
