2b738c5ce269be1990bb599161f0d72c1fc715fa
Rob Levin <roblevintennis@gmail.com>
2025-12-21 08:31:46 -0500

    chore: remove v1 directories, preparing for v2 as primary

--- agnostic-angular/.storybook/preview.js
+++
@@ -1,4 +0,0 @@
-// https://github.com/storybookjs/storybook/pull/16980/files
-// https://github.com/storybookjs/storybook/issues/16950#issuecomment-1017085928
-// This is no longer working and the common.min.css is pulled in from angular.json
-// require('!style-loader!css-loader!../libs/ag/styles/common.min.css');
--- agnostic-angular/dist/libs/ag/lib/spinner.component.d.ts
+++
@@ -1,8 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class SpinnerComponent {
-    size: 'small' | 'large' | 'xlarge' | '';
-    ariaLabel: string;
-    get spinnerClasses(): string;
-    static ɵfac: i0.ɵɵFactoryDeclaration<SpinnerComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<SpinnerComponent, "ag-spinner", never, { "size": "size"; "ariaLabel": "ariaLabel"; }, {}, never, never>;
-}
