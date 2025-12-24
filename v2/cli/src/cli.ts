e593da1165448f158b18bae48353945c2800890f
Rob Levin <roblevintennis@gmail.com>
2025-12-24 16:03:04 -0500

    Bump version for CLI

--- v2/cli/src/cli.ts
+++ v2/cli/src/cli.ts
@@ -41,7 +41,7 @@ const program = new Command();
 program
   .name('ag')
   .description('AgnosticUI Local - The UI kit that lives in your codebase')
-  .version('2.0.0-alpha.3');
+  .version('2.0.0-alpha.4');
 
 // ag init command
 program
