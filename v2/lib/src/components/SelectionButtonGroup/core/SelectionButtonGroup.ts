2b11f9b7ecef40f0a75c439b7400a56a39a20b2a
Rob Levin <roblevinillustration@gmail.com>
2026-02-01 19:45:29 -0600

    WIP: Add SelectionButton and SelectionButtonGroup components

---
+++ v2/lib/src/components/SelectionButtonGroup/core/SelectionButtonGroup.ts
@@ -0,0 +1,13 @@
+import { AgSelectionButtonGroup } from './_SelectionButtonGroup.js';
+
+if (!customElements.get('ag-selection-button-group')) {
+  customElements.define('ag-selection-button-group', AgSelectionButtonGroup);
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'ag-selection-button-group': AgSelectionButtonGroup;
+  }
+}
+
+export * from './_SelectionButtonGroup.js';
